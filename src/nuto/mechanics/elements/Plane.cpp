a13a6e6d071f0cb57cae49afa4ad5ee0f51eab08
arnold2 <arnold2@b5606e62-d4ae-482b-8a79-2ac8f2527b1c>
2010-07-09 07:38:29 +0000

    removed commented debug stuff
    git-svn-id: svn+ssh://svn.code.sf.net/p/nuto/code/trunk@286 b5606e62-d4ae-482b-8a79-2ac8f2527b1c

--- src/nuto/mechanics/elements/Plane.cpp
+++ src/nuto/mechanics/elements/Plane.cpp
@@ -98,16 +98,6 @@ void NuTo::Plane::CalculateCoefficientMatrix_0(NuTo::FullMatrix<double>& rCoeffi
 
         CalculateDerivativeShapeFunctionsLocal(derivativeShapeFunctionsNatural,invJacobian,
                                                 derivativeShapeFunctionsLocal);
-/*
-// @TODO :for now
-        NuTo::FullMatrix<double> testMat( GetNumNodes() ,2 , derivativeShapeFunctionsLocal);
-
-        DBG_POSITION_INFO("derivativeShapeFunctionsNatural")
-        DBG_PRINT_VEC(derivativeShapeFunctionsNatural)
-        DBG_POSITION_INFO("derivativeShapeFunctionsNatural")
-        DBG_PRINT_VEC(derivativeShapeFunctionsLocal)
-        testMat.Info();
-*/
 
         // determine deformation gradient from the local Displacements and the derivative of the shape functions
         // this is not included in the AddIpStiffness to avoid reallocation of the deformation gradient for each IP
