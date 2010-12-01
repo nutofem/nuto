7e5eccdce7abc77a84765cb8c44e373f18a9cb3e
eckardt4 <eckardt4@b5606e62-d4ae-482b-8a79-2ac8f2527b1c>
2010-12-01 12:45:31 +0000

    covariance matrix and Pearson's correlation matrix added for support points
    git-svn-id: svn+ssh://svn.code.sf.net/p/nuto/code/trunk@360 b5606e62-d4ae-482b-8a79-2ac8f2527b1c

--- src/nuto/metamodel/SupportPoints.cpp
+++ src/nuto/metamodel/SupportPoints.cpp
@@ -15,6 +15,8 @@
 #include <boost/ptr_container/serialize_ptr_list.hpp>
 #endif //ENABLE_SERIALIZATION
 
+#include <boost/math/distributions/normal.hpp>
+
 #include "nuto/metamodel/SupportPoints.h"
 #include "nuto/metamodel/MetamodelException.h"
 
@@ -138,10 +140,30 @@ void NuTo::SupportPoints::GetMeanValueOriginalInput(FullMatrix<double>& rMean)co
 	this->CalculateMeanValues(this->mSPOrigInput, rMean);
 }
 
+// calculate mean values of transformed inputs
+void NuTo::SupportPoints::GetMeanValueTransformedInput(FullMatrix<double>& rMean)const
+{
+	if(!this->mTransformationBuild)
+	{
+		throw NuTo::MetamodelException("[NuTo::SupportPoints::GetMeanValueOTransformedInput] transformation needs to be built first.");
+	}
+	this->CalculateMeanValues(this->mSPTransInput, rMean);
+}
+
 // calculate mean values of original outputs
 void NuTo::SupportPoints::GetMeanValueOriginalOutput(FullMatrix<double>& rMean)const
 {
 	this->CalculateMeanValues(this->mSPOrigOutput, rMean);
+}
+
+// calculate mean values of transformed outputs
+void NuTo::SupportPoints::GetMeanValueTransformedOutput(FullMatrix<double>& rMean)const
+{
+	if(!this->mTransformationBuild)
+	{
+		throw NuTo::MetamodelException("[NuTo::SupportPoints::GetMeanValueOTransformedOutput] transformation needs to be built first.");
+	}
+	this->CalculateMeanValues(this->mSPTransOutput, rMean);
 }
 
 // calculate variance of original inputs
@@ -150,10 +172,30 @@ void NuTo::SupportPoints::GetVarianceOriginalInput(FullMatrix<double>& rVariance
 	this->CalculateVariance(this->mSPOrigInput, rVariance);
 }
 
-// calculate mean values of original outputs
+// calculate variance of transformed inputs
+void NuTo::SupportPoints::GetVarianceTransformedInput(FullMatrix<double>& rVariance)const
+{
+	if(!this->mTransformationBuild)
+	{
+		throw NuTo::MetamodelException("[NuTo::SupportPoints::GetVarianceTransformedInput] transformation needs to be built first.");
+	}
+	this->CalculateVariance(this->mSPTransInput, rVariance);
+}
+
+// calculate variance of original outputs
 void NuTo::SupportPoints::GetVarianceOriginalOutput(FullMatrix<double>& rVariance)const
 {
 	this->CalculateVariance(this->mSPOrigOutput, rVariance);
+}
+
+// calculate variance of transformed outputs
+void NuTo::SupportPoints::GetVarianceTransformedOutput(FullMatrix<double>& rVariance)const
+{
+	if(!this->mTransformationBuild)
+	{
+		throw NuTo::MetamodelException("[NuTo::SupportPoints::GetVarianceTransformedOutput] transformation needs to be built first.");
+	}
+	this->CalculateVariance(this->mSPTransOutput, rVariance);
 }
 
 // calculate covariance matrix
@@ -162,13 +204,77 @@ void NuTo::SupportPoints::GetCovarianceMatrixOriginal(FullMatrix<double>& rCovar
 	this->CalculateCovarianceMatrix(this->mSPOrigInput, this->mSPOrigOutput, rCovarianceMatrix);
 }
 
-// calculate Pearson's correlation matrix
+// calculate covariance matrix
+void NuTo::SupportPoints::GetCovarianceMatrixTransformed(FullMatrix<double>& rCovarianceMatrix) const
+{
+	if(!this->mTransformationBuild)
+	{
+		throw NuTo::MetamodelException("[NuTo::SupportPoints::GetCovarianceMatrixTransformed] transformation needs to be built first.");
+	}
+	this->CalculateCovarianceMatrix(this->mSPTransInput, this->mSPTransOutput, rCovarianceMatrix);
+}
+
+// calculate Pearson's correlation matrix using original support point coordinates
 void NuTo::SupportPoints::GetPearsonCorrelationMatrixOriginal(FullMatrix<double>& rCorrelationMatrix) const
 {
 	this->CalculatePearsonCorrelationMatrix(this->mSPOrigInput, this->mSPOrigOutput, rCorrelationMatrix);
 }
 
+// calculate Pearson's correlation matrix using transformed support point coordinates
+void NuTo::SupportPoints::GetPearsonCorrelationMatrixTransformed(FullMatrix<double>& rCorrelationMatrix) const
+{
+	if(!this->mTransformationBuild)
+	{
+		throw NuTo::MetamodelException("[NuTo::SupportPoints::GetPearsonCorrelationMatrixTransformed] transformation needs to be built first.");
+	}
+	this->CalculatePearsonCorrelationMatrix(this->mSPTransInput, this->mSPTransOutput, rCorrelationMatrix);
+}
+
+// calculate the confidence interval on Pearson's correlation coefficient using original support point coordinates
+void NuTo::SupportPoints::GetPearsonCorrelationMatrixConfidenceIntervalsOriginal(FullMatrix<double>& rCorrelationMatrix, FullMatrix<double>& rMinCorrelationMatrix, FullMatrix<double>& rMaxCorrelationMatrix, double rAlpha) const
+{
+	try
+	{
+		this->CalculatePearsonCorrelationMatrixConfidenceIntervals(this->mSPOrigInput, this->mSPOrigOutput, rCorrelationMatrix, rMinCorrelationMatrix, rMaxCorrelationMatrix, rAlpha);
+	}
+	catch(NuTo::Exception& e)
+	{
+		NuTo::MetamodelException myException(e.ErrorMessage());
+		myException.AddMessage("[NuTo::SupportPoints::GetPearsonCorrelationMatrixConfidenceIntervalsOriginal] error calculating confidence intervals on Person's correlation coefficient.");
+		throw myException;
+	}
+
+}
+
+// calculate the confidence interval on Pearson's correlation coefficient using transformed support point coordinates
+void NuTo::SupportPoints::GetPearsonCorrelationMatrixConfidenceIntervalsTransformed(FullMatrix<double>& rCorrelationMatrix, FullMatrix<double>& rMinCorrelationMatrix, FullMatrix<double>& rMaxCorrelationMatrix, double rAlpha) const
+{
+	if(!this->mTransformationBuild)
+	{
+		throw NuTo::MetamodelException("[NuTo::SupportPoints::GetPearsonCorrelationMatrixConfidenceIntervalsTransformed] transformation needs to be built first.");
+	}
+	try
+	{
+		this->CalculatePearsonCorrelationMatrixConfidenceIntervals(this->mSPTransInput, this->mSPTransOutput, rCorrelationMatrix, rMinCorrelationMatrix, rMaxCorrelationMatrix, rAlpha);
+	}
+	catch(NuTo::Exception& e)
+	{
+		NuTo::MetamodelException myException(e.ErrorMessage());
+		myException.AddMessage("[NuTo::SupportPoints::GetPearsonCorrelationMatrixConfidenceIntervalsTransformed] error calculating confidence intervals on Person's correlation coefficient.");
+		throw myException;
+	}
+
+}
+
 // calculate mean values
+/*!
+ * The sample mean is separately calculated for each point coordinate \f$x_{i\ldots}\f$.
+ * For the \f$i\f$-th coordinate the sample mean \f$\bar{x}_i\f$ is defined as
+ * \f[
+ *  \bar{x}_i = \dfrac{1}{n} \sum_{j=1}^n x_{ij}, \qquad i = 1 \ldots p,
+ * \f]
+ * where \f$p\f$ is the number of point coordinates, \f$n\f$ is the number of samples.
+ */
 void NuTo::SupportPoints::CalculateMeanValues(const FullMatrix<double>& rData, FullMatrix<double>& rMean) const
 {
 	int numRows = rData.GetNumRows();
@@ -195,6 +301,15 @@ void NuTo::SupportPoints::CalculateMeanValues(const FullMatrix<double>& rData, F
 }
 
 // calculate variances
+/*!
+ * The sample variance is separately calculated for each point coordinate \f$x_{i\ldots}\f$.
+ * For the \f$i\f$-th coordinate the sample variance \f$\sigma_{{x}_i}^2\f$ is defined as
+ * \f[
+ *  \sigma_{{x}_i}^2 = \dfrac{1}{n - 1} \sum_{j=1}^n \left[x_{ij} - \bar{x}_i\right]^2, \qquad i = 1 \ldots p,
+ * \f]
+ * where \f$p\f$ is the number of point coordinates, \f$n\f$ is the number of samples, and \f$\bar{x}_i\f$ is the sample mean of the
+ * corresponding coordinate.
+ */
 void NuTo::SupportPoints::CalculateVariance(const FullMatrix<double>& rData, FullMatrix<double>& rVariance) const
 {
 	int numRows = rData.GetNumRows();
@@ -227,6 +342,27 @@ void NuTo::SupportPoints::CalculateVariance(const FullMatrix<double>& rData, Ful
 }
 
 // calculate covariance matrix
+/*!
+ * The sample covariance matrix \f$\boldsymbol{COV}\f$ of the support points is defined as
+ * \f[
+ *  \boldsymbol{COV} = \begin{bmatrix}
+ *    cov(x_1,x_1) & cov(x_1, x_2) & \cdots & cov(x_1, x_{p_i}) & cov(x_1, y_1) & cov(x_1, y_2) & \cdots & cov(x_1, y_{p_o})\\
+ *    cov(x_2,x_1) & cov(x_2, x_2) & \cdots & cov(x_2, x_{p_i}) & cov(x_2, y_1) & cov(x_2, y_2) & \cdots & cov(x_2, y_{p_o})\\
+ *    \vdots & \vdots & \ddots & \vdots & \vdots & \vdots & \ddots & \vdots\\
+ *    cov(x_{p_i},x_1) & cov(x_{p_i}, x_2) & \cdots & cov(x_{p_i}, x_{p_i}) & cov(x_{p_i}, y_1) & cov(x_{p_i}, y_2) & \cdots & cov(x_{p_i}, y_{p_o})\\
+ *    cov(y_1,x_1) & cov(y_1, x_2) & \cdots & cov(y_1, x_{p_i}) & cov(y_1, y_1) & cov(y_1, y_2) & \cdots & cov(y_1, y_{p_o})\\
+ *    cov(y_2,x_1) & cov(y_2, x_2) & \cdots & cov(y_2, x_{p_i}) & cov(y_2, y_1) & cov(y_2, y_2) & \cdots & cov(y_2, y_{p_o})\\
+ *    \vdots & \vdots & \ddots & \vdots & \vdots & \vdots & \ddots & \vdots\\
+ *    cov(y_{p_o},x_1) & cov(y_{p_o}, x_2) & \cdots & cov(y_{p_o}, x_{p_i}) & cov(y_{p_o}, y_1) & cov(y_{p_o}, y_2) & \cdots & cov(y_{p_o}, y_{p_o})
+ *  \end{bmatrix},
+ * \f]
+ * with
+ * \f[
+ *   cov(z_i,z_j) = \dfrac{1}{n-1} \sum_{k=1}^n (z_{ik} - \bar{z}_i)(z_{jk} - \bar{z}_j),
+ * \f]
+ * where \f$\bar{z}_i\f$ is the sample mean of the \f$i\f$-th coordinate, \f$n\f$ is the number of samples, \f$p_i\f$ is the number of input coordinates,
+ * and \f$p_o\f$ is the number of output coordinates. Since \f$cov(z_i,z_j) = cov(z_j,z_i)\f$ the covariance matrix is symmetric.
+ */
 void NuTo::SupportPoints::CalculateCovarianceMatrix(const FullMatrix<double>& rInputData, const FullMatrix<double>& rOutputData, FullMatrix<double>& rCovarianceMatrix) const
 {
 	// get data
@@ -317,6 +453,29 @@ void NuTo::SupportPoints::CalculateCovarianceMatrix(const FullMatrix<double>& rI
 	}
 }
 
+// calculate Pearson's sample correlation matrix
+/*!
+ * Pearson's sample correlation matrix \f$\boldsymbol{R}\f$ is defined as
+ * \f[
+ *  \boldsymbol{R} = \begin{bmatrix}
+ *    \rho_{x_1,x_1} & \rho_{x_1, x_2} & \cdots & \rho_{x_1, x_{p_i}} & \rho_{x_1, y_1} & \rho_{x_1, y_2} & \cdots & \rho_{x_1, y_{p_o}}\\
+ *    \rho_{x_2,x_1} & \rho_{x_2, x_2} & \cdots & \rho_{x_2, x_{p_i}} & \rho_{x_2, y_1} & \rho_{x_2, y_2} & \cdots & \rho_{x_2, y_{p_o}}\\
+ *    \vdots & \vdots & \ddots & \vdots & \vdots & \vdots & \ddots & \vdots\\
+ *    \rho_{x_{p_i},x_1} & \rho_{x_{p_i}, x_2} & \cdots & \rho_{x_{p_i}, x_{p_i}} & \rho_{x_{p_i}, y_1} & \rho_{x_{p_i}, y_2} & \cdots & \rho_{x_{p_i}, y_{p_o}}\\
+ *    \rho_{y_1,x_1} & \rho_{y_1, x_2} & \cdots & \rho_{y_1, x_{p_i}} & \rho_{y_1, y_1} & \rho_{y_1, y_2} & \cdots & \rho_{y_1, y_{p_o}}\\
+ *    \rho_{y_2,x_1} & \rho_{y_2, x_2} & \cdots & \rho_{y_2, x_{p_i}} & \rho_{y_2, y_1} & \rho_{y_2, y_2} & \cdots & \rho_{y_2, y_{p_o}}\\
+ *    \vdots & \vdots & \ddots & \vdots & \vdots & \vdots & \ddots & \vdots\\
+ *    \rho_{y_{p_o},x_1} & \rho_{y_{p_o}, x_2} & \cdots & \rho_{y_{p_o}, x_{p_i}} & \rho_{y_{p_o}, y_1} & \rho_{y_{p_o}, y_2} & \cdots & \rho_{y_{p_o}, y_{p_o}}
+ *  \end{bmatrix}
+ * \f]
+ * with the sample correlation coefficient \f$\rho_{z_i,z_j}\f$ given by
+ * \f[
+ * \rho_{z_i,z_j} = \dfrac{cov(z_i,z_j)}{\sigma_{z_i} \sigma_{z_j}} = \dfrac{1}{n-1} \sum_{k=1}^n \left[\dfrac{z_{ik} - \bar{z}_i}{\sigma_{z_i}} \right]\left[\dfrac{z_{jk} - \bar{z}_j}{\sigma_{z_j}} \right],
+ * \f]
+ * where \f$cov(z_i,z_j)\f$ is the covariance of the variables \f$z_i\f$ and \f$z_j\f$, \f$\sigma_{z_i}, \sigma_{z_j}\f$ are the corresponding sample standard deviations,
+ * \f$\bar{z}_i, \bar{z}_j\f$ are the corresponding sample means, \f$n\f$ is the number of samples, \f$p_i\f$ is the number of input coordinates,
+ * and \f$p_o\f$ is the number of output coordinates.
+ */
 void NuTo::SupportPoints::CalculatePearsonCorrelationMatrix(const FullMatrix<double>& rInputData, const FullMatrix<double>& rOutputData, FullMatrix<double>& rCorrelationMatrix) const
 {
 	// calculate covariance matrix
@@ -331,7 +490,6 @@ void NuTo::SupportPoints::CalculatePearsonCorrelationMatrix(const FullMatrix<dou
 	{
 		assert(rCorrelationMatrix.GetValue(dimCount,dimCount) > 0.0);
 		stddev[dimCount] = sqrt(rCorrelationMatrix.GetValue(dimCount,dimCount));
-		std::cout << "stddev: " << stddev[dimCount] << std::endl;
 	}
 
 	// scale covariance matrix
@@ -343,6 +501,53 @@ void NuTo::SupportPoints::CalculatePearsonCorrelationMatrix(const FullMatrix<dou
 		}
 	}
 }
+
+// calculate the confidence interval on the coefficients of Pearson's correlation matrix
+/*!
+ *
+ */
+void NuTo::SupportPoints::CalculatePearsonCorrelationMatrixConfidenceIntervals(const FullMatrix<double>& rInputData, const FullMatrix<double>& rOutputData, FullMatrix<double>& rCorrelationMatrix, FullMatrix<double>& rMinCorrelationMatrix, FullMatrix<double>& rMaxCorrelationMatrix, double rAlpha ) const
+{
+	// check number of samples
+	if(this->GetNumSupportPoints() < 4)
+	{
+		throw NuTo::MetamodelException("[NuTo::SupportPoints::CalculatePearsonCorrelationMatrixConfidenceIntervals] at least 4 support points are required for the calculation of the confidence interval on correlation coefficients.");
+	}
+
+	try
+	{
+		// calculate Pearson's correlation matrix
+		this->CalculatePearsonCorrelationMatrix(rInputData, rOutputData, rCorrelationMatrix);
+
+		// calculate the (1-0.5 * rAlpha)-quantile of the standard normal distribution
+		boost::math::normal stdNormalDistribution;
+		double quantile = boost::math::quantile(boost::math::complement(stdNormalDistribution,0.5 * rAlpha));
+		double delta = quantile/sqrt(static_cast<double>(this->GetNumSupportPoints() - 3));
+
+		// copy correlation matrix
+		rMinCorrelationMatrix = rCorrelationMatrix;
+		rMaxCorrelationMatrix = rCorrelationMatrix;
+
+		// calculate confidence interval
+		for(int row = 0; row < this->GetDimInput() + this->GetDimOutput(); row++)
+		{
+			for(int col = row + 1; col < this->GetDimInput() + this->GetDimOutput(); col++)
+			{
+				rMinCorrelationMatrix(row,col) = tanh(atanh(rCorrelationMatrix(row,col)) - delta);
+				rMinCorrelationMatrix(col,row) = rMinCorrelationMatrix(row,col);
+				rMaxCorrelationMatrix(row,col) = tanh(atanh(rCorrelationMatrix(row,col)) + delta);
+				rMaxCorrelationMatrix(col,row) = rMaxCorrelationMatrix(row,col);
+			}
+		}
+	}
+	catch(NuTo::Exception& e)
+	{
+		NuTo::MetamodelException myException(e.ErrorMessage());
+		myException.AddMessage("[NuTo::SupportPoints::CalculatePearsonCorrelationMatrixConfidenceIntervals] error calculating confidence intervals on correlation coefficients.");
+		throw myException;
+	}
+}
+
 
 #ifdef ENABLE_SERIALIZATION
 // serializes the class
--- src/nuto/metamodel/SupportPoints.h
+++ src/nuto/metamodel/SupportPoints.h
@@ -147,26 +147,63 @@ public:
     //! @param rMean ... vector of mean values (output)
     void GetMeanValueOriginalInput(FullMatrix<double>& rMean)const;
 
+    //! @brief calculate the mean value of the transformed inputs
+    //! @param rMean ... vector of mean values (output)
+    void GetMeanValueTransformedInput(FullMatrix<double>& rMean)const;
+
     //! @brief calculate the mean value of the original outputs
     //! @param rMean ... vector of mean values (output)
     void GetMeanValueOriginalOutput(FullMatrix<double>& rMean)const;
+
+    //! @brief calculate the mean value of the transformed outputs
+    //! @param rMean ... vector of mean values (output)
+    void GetMeanValueTransformedOutput(FullMatrix<double>& rMean)const;
 
     //! @brief calculate the variance of the original inputs
     //! @param rVariance ... vector of variances (output)
     void GetVarianceOriginalInput(FullMatrix<double>& rVariance)const;
 
-    //! @brief calculate the variance of the original outputs
+    //! @brief calculate the variance of the transformed inputs
+    //! @param rVariance ... vector of variances (output)
+    void GetVarianceTransformedInput(FullMatrix<double>& rVariance)const;
+
+    //! @brief calculate the variance of the transformed outputs
     //! @param rVariance ... vector of variances (output)
     void GetVarianceOriginalOutput(FullMatrix<double>& rVariance)const;
+
+    //! @brief calculate the variance of the original outputs
+    //! @param rVariance ... vector of variances (output)
+    void GetVarianceTransformedOutput(FullMatrix<double>& rVariance)const;
 
     //! @brief calculate the covariance matrix of original inputs and outputs
     //! @param rCovarianceMatrix ... covariance matrix
     void GetCovarianceMatrixOriginal(FullMatrix<double>& rCovarianceMatrix) const;
 
+    //! @brief calculate the covariance matrix of transformed inputs and outputs
+    //! @param rCovarianceMatrix ... covariance matrix
+    void GetCovarianceMatrixTransformed(FullMatrix<double>& rCovarianceMatrix) const;
+
     //! @brief calculate Pearson's correlation matrix
-    //! @param rCorrelationMatrix  ... Pearsons correlation matrix
+    //! @param rCorrelationMatrix  ... Pearson's correlation matrix using original support point coordinates
     void GetPearsonCorrelationMatrixOriginal(FullMatrix<double>& rCorrelationMatrix) const;
 
+    //! @brief calculate Pearson's correlation matrix using transformed support point coordinates
+    //! @param rCorrelationMatrix  ... Pearsons correlation matrix
+    void GetPearsonCorrelationMatrixTransformed(FullMatrix<double>& rCorrelationMatrix) const;
+
+    //! @brief calculate the confidence interval on Pearson's correlation coefficient using original support point coordinates
+    //! @param rCorrelationMatrix  ... Pearson's correlation matrix using original support point coordinates
+	//! @param rMinCorrelationMatrix ... lower bound of the confidence interval on the coefficients of Pearson's correlation matrix
+	//! @param rCorrelationMatrix ... upper bound of the confidence interval on the coefficients of Pearson's correlation matrix
+	//! @param rAlpha ... the confidence level is defined as (1-rAlpha)
+    void GetPearsonCorrelationMatrixConfidenceIntervalsOriginal(FullMatrix<double>& rCorrelationMatrix, FullMatrix<double>& rMinCorrelationMatrix, FullMatrix<double>& rMaxCorrelationMatrix, double rAlpha = 0.05) const;
+
+    //! @brief calculate the confidence interval on Pearson's correlation coefficient using transformed support point coordinates
+    //! @param rCorrelationMatrix  ... Pearson's correlation matrix using original support point coordinates
+	//! @param rMinCorrelationMatrix ... lower bound of the confidence interval on the coefficients of Pearson's correlation matrix
+	//! @param rCorrelationMatrix ... upper bound of the confidence interval on the coefficients of Pearson's correlation matrix
+	//! @param rAlpha ... the confidence level is defined as (1-rAlpha)
+    void GetPearsonCorrelationMatrixConfidenceIntervalsTransformed(FullMatrix<double>& rCorrelationMatrix, FullMatrix<double>& rMinCorrelationMatrix, FullMatrix<double>& rMaxCorrelationMatrix, double rAlpha = 0.05) const;
 private:
     FullMatrix<double>  mSPOrigInput;      //!< original inputs, each sample after another
     FullMatrix<double>  mSPOrigOutput;     //!< original outputs, each sample after another
@@ -201,6 +238,15 @@ private:
 	//! @param rOutputData ... matrix of output data points, each sample after another
 	//! @param rCorrelationMatrix ... Pearson's correlation matrix
 	void CalculatePearsonCorrelationMatrix(const FullMatrix<double>& rInputData, const FullMatrix<double>& rOutputData, FullMatrix<double>& rCorrelationMatrix) const;
+
+	//! @brief calculate Pearson's correlation matrix
+	//! @param rInputData ... matrix of input data points, each sample after another
+	//! @param rOutputData ... matrix of output data points, each sample after another
+	//! @param rCorrelationMatrix ... Pearson's correlation matrix
+	//! @param rMinCorrelationMatrix ... lower bound of the confidence interval on the coefficients of Pearson's correlation matrix
+	//! @param rCorrelationMatrix ... upper bound of the confidence interval on the coefficients of Pearson's correlation matrix
+	//! @param rAlpha ... the confidence level is defined as (1-rAlpha)
+	void CalculatePearsonCorrelationMatrixConfidenceIntervals(const FullMatrix<double>& rInputData, const FullMatrix<double>& rOutputData, FullMatrix<double>& rCorrelationMatrix, FullMatrix<double>& rMinCorrelationMatrix, FullMatrix<double>& rMaxCorrelationMatrix, double rAlpha) const;
 
 };
 } // namespace nuto
