5cef939f59c3eaba13a8263b52d21de68291c102
res2002 <res2002@b5606e62-d4ae-482b-8a79-2ac8f2527b1c>
2011-03-04 16:39:22 +0000

    Helper class to get a wxGraphicsContext for a wxBitmap
    git-svn-id: svn+ssh://svn.code.sf.net/p/nuto/code/trunk@432 b5606e62-d4ae-482b-8a79-2ac8f2527b1c

---
+++ src/gui/src/UICommon/BitmapGraphicsContext.cpp
@@ -0,0 +1,67 @@
+/**\file
+ * Helper class to get a wxGraphicsContext for a wxBitmap
+ */
+/*
+ * Written 2011 by Frank Richter <frank.richter@gmail.com>
+ * For Bauhaus University Weimar, Institute of Structural Mechanics
+ *
+ */
+
+#include "common.h"
+#include "uicommon/BitmapGraphicsContext.h"
+
+#include <wx/graphics.h>
+#include <wx/rawbmp.h>
+
+namespace uicommon
+{
+  BitmapGraphicsContext::BitmapGraphicsContext (wxBitmap& bmp, ClearMode clearMode)
+    : theGC (nullptr), bmp (bmp)
+  {
+    alphaHack = false;
+    if (clearMode == clear)
+    {
+    #if defined(__WXGTK__)
+      alphaHack = true;
+    #endif
+    
+      // Clear bitmap data.
+      wxAlphaPixelData bmpPixels (bmp);
+      wxAlphaPixelData::Iterator p (bmpPixels);
+      for (size_t numPix = bmpPixels.GetWidth() * bmpPixels.GetHeight(); numPix-- > 0; )
+      {
+	p.Red() = alphaHack ? 1 : 0;
+	p.Green() = alphaHack ? 1 : 0;
+	p.Blue() = alphaHack ? 1 : 0;
+	p.Alpha() = 0;
+	++p;
+      }
+    }
+    
+    dc.SelectObject (bmp);
+    theGC = wxGraphicsContext::Create (dc);
+  }
+
+  BitmapGraphicsContext::~BitmapGraphicsContext()
+  {
+    delete theGC;
+    dc.SelectObject (wxNullBitmap);
+    
+    if (alphaHack)
+    {
+      // Horrible, horrible - create *some* alpha by using a mask color
+      wxAlphaPixelData bmpPixels (bmp);
+      wxAlphaPixelData::Iterator p (bmpPixels);
+      for (int y = 0; y < bmpPixels.GetHeight(); y++)
+      {
+	for (int x = 0; x < bmpPixels.GetWidth(); x++)
+	{
+	  if ((p.Red() != 1) || (p.Green() != 1) || (p.Blue() != 1))
+	    p.Alpha() = 255;
+	++p;
+	}
+      }
+    }
+  }
+  
+} // namespace uicommon
