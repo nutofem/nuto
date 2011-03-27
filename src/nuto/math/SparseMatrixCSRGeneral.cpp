cf1a90c76fc1ab1ef97094b52dace8672f38648a
res2002 <res2002@b5606e62-d4ae-482b-8a79-2ac8f2527b1c>
2011-03-27 19:15:08 +0000

    Move some Save() and Restore() implementations from .hpp to .cpp files. Reduces size of build files.
    git-svn-id: svn+ssh://svn.code.sf.net/p/nuto/code/trunk@494 b5606e62-d4ae-482b-8a79-2ac8f2527b1c

--- src/nuto/math/SparseMatrixCSRGeneral.cpp
+++ src/nuto/math/SparseMatrixCSRGeneral.cpp
@@ -372,4 +372,139 @@ void SparseMatrixCSRGeneral<double>::Gauss(FullMatrix<double>& rRhs, std::vector
     this->ReorderColumns(rMappingInitialToNewOrdering);
 }
 
+#ifdef ENABLE_SERIALIZATION
+template <class T>
+void SparseMatrixCSRGeneral<T>::Save ( const std::string &filename, std::string rType)const
+{
+	try
+	 {
+		 //transform to uppercase
+		 std::transform(rType.begin(), rType.end(), rType.begin(), (int(*)(int))toupper);
+
+		 // open file
+		 std::ofstream ofs ( filename.c_str(), std::ios_base::binary );
+		 if(! ofs.is_open())
+		 {
+			 throw MathException("[NuTo::SparseMatrixCSRGeneral::Save] Error opening file.");
+		 }
+
+		 // write data to file
+		 std::string typeIdString(this->GetTypeId());
+		 if (rType=="BINARY")
+		 {
+			 boost::archive::binary_oarchive oba ( ofs, std::ios::binary );
+			 oba & boost::serialization::make_nvp ("Object_type", typeIdString );
+			 oba & boost::serialization::make_nvp(typeIdString.c_str(), *this);
+		 }
+		 else if (rType=="XML")
+		 {
+			 boost::archive::xml_oarchive oxa ( ofs, std::ios::binary );
+			 std::string tmpString(this->GetTypeId());
+			 oxa & boost::serialization::make_nvp ("Object_type", typeIdString );
+			 oxa & boost::serialization::make_nvp(typeIdString.c_str(), *this);
+		 }
+		 else if (rType=="TEXT")
+		 {
+			 boost::archive::text_oarchive ota ( ofs, std::ios::binary );
+			 ota & boost::serialization::make_nvp("Object_type", typeIdString );
+			 ota & boost::serialization::make_nvp(typeIdString.c_str(), *this);
+		 }
+		 else
+		 {
+			 throw MathException ( "[NuTo::SparseMatrixCSRGeneral::Save] File type not implemented." );
+		 }
+
+		 // close file
+		 ofs.close();
+	 }
+	 catch ( boost::archive::archive_exception e )
+	 {
+		 std::string s ( std::string ( "[NuTo::SparseMatrixCSRGeneral::Save]File save exception in boost - " ) + std::string ( e.what() ) );
+		 throw MathException ( s );
+	 }
+	 catch ( MathException &e )
+	 {
+		 throw e;
+	 }
+	 catch ( std::exception &e )
+	 {
+		 throw MathException ( e.what() );
+	 }
+}
+
+template <class T>
+void SparseMatrixCSRGeneral<T>::Restore ( const std::string &filename,  std::string rType)
+{
+	try
+	{
+		//transform to uppercase
+		std::transform(rType.begin(), rType.end(), rType.begin(), (int(*)(int))toupper);
+
+		// open file
+		std::ifstream ifs ( filename.c_str(), std::ios_base::binary );
+		if(! ifs.is_open())
+		{
+			throw MathException("[NuTo::SparseMatrixCSRGeneral::Restore] Error opening file.");
+		}
+
+		std::string typeIdString;
+		if (rType=="BINARY")
+		{
+			boost::archive::binary_iarchive oba ( ifs, std::ios::binary );
+			oba & boost::serialization::make_nvp ( "Object_type", typeIdString );
+			if ( typeIdString != this->GetTypeId() )
+			{
+				throw MathException ( "[NuTo::SparseMatrixCSRGeneral::Restore] Data type of object in file ("+typeIdString+") is not identical to data type of object to read ("+this->GetTypeId() +")." );
+			}
+			oba & boost::serialization::make_nvp(typeIdString.c_str(), *this);
+		}
+		else if (rType=="XML")
+		{
+			boost::archive::xml_iarchive oxa ( ifs, std::ios::binary );
+			oxa & boost::serialization::make_nvp ( "Object_type", typeIdString );
+			if ( typeIdString != this->GetTypeId() )
+			{
+				throw MathException ( "[NuTo::SparseMatrixCSRGeneral::Restore] Data type of object in file ("+typeIdString+") is not identical to data type of object to read ("+this->GetTypeId() +")." );
+			}
+			oxa & boost::serialization::make_nvp(typeIdString.c_str(), *this);
+		}
+		else if (rType=="TEXT")
+		{
+			boost::archive::text_iarchive ota ( ifs, std::ios::binary );
+			ota & boost::serialization::make_nvp ( "Object_type", typeIdString );
+			if ( typeIdString != this->GetTypeId() )
+			{
+				throw MathException ( "[NuTo::SparseMatrixCSRGeneral::Restore] Data type of object in file ("+typeIdString+") is not identical to data type of object to read ("+this->GetTypeId() +")." );
+			}
+			ota & boost::serialization::make_nvp(typeIdString.c_str(), *this);
+		}
+		else
+		{
+			throw MathException ( "[NuTo::SparseMatrixCSRGeneral::Restore]File type not implemented" );
+		}
+		// close file
+		ifs.close();
+	}
+	catch ( boost::archive::archive_exception e )
+	{
+		std::string s ( std::string ( "[NuTo::SparseMatrixCSRGeneral::Restore] File save exception in boost - " ) + std::string ( e.what() ) );
+		throw MathException ( s );
+	}
+	catch ( MathException &e )
+	{
+		throw e;
+	}
+	catch ( std::exception &e )
+	{
+		throw MathException ( e.what() );
+	}
+}
+
+template void SparseMatrixCSRGeneral<int>::Save (const std::string &, std::string) const;
+template void SparseMatrixCSRGeneral<int>::Restore (const std::string &, std::string);
+template void SparseMatrixCSRGeneral<double>::Save (const std::string &, std::string) const;
+template void SparseMatrixCSRGeneral<double>::Restore (const std::string &, std::string);
+
+#endif // ENABLE_SERIALIZATION
+
 }
