55c06a2ecb632b6bd1143f77edad42ee537cfe38
ttitscher <ttitscher@b5606e62-d4ae-482b-8a79-2ac8f2527b1c>
2014-01-14 09:42:10 +0000

    Added src/nuto/geometryConcrete
    git-svn-id: svn+ssh://svn.code.sf.net/p/nuto/code/trunk@658 b5606e62-d4ae-482b-8a79-2ac8f2527b1c

---
+++ src/nuto/geometryConcrete/collision/HelloWorld.cpp
@@ -0,0 +1,19 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: 13 Jan 2014
+ *      Author: ttitsche
+ */
+
+#include "nuto/geometryConcrete/collision/HelloWorld.h"
+#include <iostream>
+namespace NuTo
+{
+
+}
+
+void NuTo::HelloWorld::PrintHelloWorld()
+{
+	std::cout << "Hello World, this my first NuTo print!" << std::endl;
+}
+/* namespace NuTo */
---
+++ src/nuto/geometryConcrete/collision/HelloWorld.h
@@ -0,0 +1,19 @@
+/*
+ * HelloWorld.h
+ *
+ *  Created on: 13 Jan 2014
+ *      Author: ttitsche
+ */
+
+#ifndef HELLOWORLD_H_
+#define HELLOWORLD_H_
+
+namespace NuTo {
+
+class HelloWorld {
+public:
+	void PrintHelloWorld();
+};
+
+} /* namespace NuTo */
+#endif /* HELLOWORLD_H_ */
