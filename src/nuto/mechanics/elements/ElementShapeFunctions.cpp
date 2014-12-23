eb79550a5c9acfb4fd59391f278c7b7dfcdd438f
ttitscher <ttitscher@b5606e62-d4ae-482b-8a79-2ac8f2527b1c>
2014-12-23 15:13:34 +0000

    Added cubic damage law
    Added 1D4N shape functions
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/nuto/code/trunk@723 b5606e62-d4ae-482b-8a79-2ac8f2527b1c

--- src/nuto/mechanics/elements/ElementShapeFunctions.cpp
+++ src/nuto/mechanics/elements/ElementShapeFunctions.cpp
@@ -5,7 +5,7 @@
 
 namespace NuTo
 {
-namespace ShapeFunctions1D
+namespace ShapeFunctions1D // interval -1 to 1
 {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void ShapeFunctions1D2N(double rLocalCoordinates, std::vector<double>& rShapeFunctions)
@@ -42,7 +42,25 @@ namespace ShapeFunctions1D
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    void ShapeFunctions1D4N(double rLocalCoordinates, std::vector<double>& rShapeFunctions)
+    {
+        assert(rShapeFunctions.size()==4);
+        double x = rLocalCoordinates;
+        rShapeFunctions[0] = -  9./16. * (x+1./3) * (x-1./3) * (x-1);
+        rShapeFunctions[1] =   27./16. * (x+1.)   * (x-1./3) * (x-1);
+        rShapeFunctions[2] = - 27./16  * (x+1.)   * (x+1./3) * (x-1);
+        rShapeFunctions[3] =    9./16. * (x-1./3) * (x+1./3) * (x+1);
+    }
 
+    void DerivativeShapeFunctions1D4N(double rLocalCoordinates, std::vector<double>& rDerivativeShapeFunctions)
+    {
+        assert(rDerivativeShapeFunctions.size()==4);
+        double x = rLocalCoordinates;
+        rDerivativeShapeFunctions[0] = (-27.0*x*x + 18.0*x +  1.0) / 16.;
+        rDerivativeShapeFunctions[1] = ( 81.0*x*x - 18.0*x - 27.0) / 16.;
+        rDerivativeShapeFunctions[2] = (-81.0*x*x - 18.0*x + 27.0) / 16.;
+        rDerivativeShapeFunctions[3] = ( 27.0*x*x + 18.0*x -  1.0) / 16.;
+    }
 }
 
 namespace ShapeFunctions2D
