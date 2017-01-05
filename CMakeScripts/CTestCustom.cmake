6dd5000c0341c0e155937093d38aeec13f98a28a
Christoph Pohl <christophpohl@gmx.com>
2017-01-05 17:32:05 +0100

    Add ability to ignore specific tests
    Previously, when you had a failed test after a change, you could either
    let Travis fail or comment out all CMake code belonging to the test. Now
    you can simply add it to the file `CMakeScripts/CTestCustom.cmake` and
    ctest will skip that test.

---
+++ CMakeScripts/CTestCustom.cmake
@@ -0,0 +1,5 @@
+# Here, you can insert tests you want ctest to skip, e.g. when you have made
+# a change, but were unable to get all the tests to run correctly
+set(CTEST_CUSTOM_TESTS_IGNORE
+    #p::mechanics::Brick8N
+    )
