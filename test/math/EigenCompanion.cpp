b6576f0427f3d4221dc77d971055b3151e6d8469
Thomas Titscher <thomas.titscher@gmail.com>
2017-01-06 17:28:06 +0100

    Add WriteToFile and ReadFromFile to Companion

--- test/math/EigenCompanion.cpp
+++ test/math/EigenCompanion.cpp
@@ -25,3 +25,14 @@ BOOST_AUTO_TEST_CASE(AppendRows)
     Eigen::MatrixXd wrongBottom = Eigen::Matrix<double, 1, 3>::Ones();
     BOOST_CHECK_THROW(NuTo::EigenCompanion::AppendRows(top, wrongBottom), NuTo::MathException);
 }
+
+BOOST_AUTO_TEST_CASE(ReadWriteFile)
+{
+    Eigen::MatrixXd toFile(2,2);
+    toFile << 1,2,3,4;
+    NuTo::EigenCompanion::WriteToFile(toFile, "EigenCompanionFile.dat");
+
+    Eigen::MatrixXd fromFile = NuTo::EigenCompanion::ReadFromFile("EigenCompanionFile.dat");
+
+    BOOST_CHECK_CLOSE((toFile - fromFile).norm(), 0, 1.e-10);
+}
