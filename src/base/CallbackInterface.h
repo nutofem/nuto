65741fb5ad89941ab109f3202ff248445707f692
Christoph Pohl <christophpohl@gmx.com>
2017-01-09 13:11:09 +0100

    Remove constness from CallbackInterface::Exit

--- src/base/CallbackInterface.h
+++ src/base/CallbackInterface.h
@@ -39,7 +39,7 @@ public:
 
     //! @brief Exit function, returns true if a certain state of the structure is reached
     //! @param StructureBase& ... not const since most of the useful methods require an Evaluate, which is non-const
-    virtual bool Exit(StructureBase&) const;
+    virtual bool Exit(StructureBase&);
 };
 
 } /* namespace NuTo */
