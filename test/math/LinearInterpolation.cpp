e45446c14171610125bc266a9fc0b9a8005be3d5
Christoph Pohl <christophpohl@gmx.com>
2017-02-02 16:10:23 +0100

    Rearrange math tests
    Same as with the mechanics tests: seperate integration from unit tests.

--- test/math/LinearInterpolation.cpp
+++ test/math/LinearInterpolation.cpp
@@ -1,11 +1,10 @@
-#define BOOST_TEST_MODULE InterpolationTest
+#define BOOST_TEST_MODULE LinearInterpolation
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 
 #include <array>
 #include "math/LinearInterpolation.h"
-#include "math/CubicSplineInterpolation.h"
 #include "math/MathException.h"
 
 // needed for building with clang when boost test has been built with gcc
@@ -59,32 +58,4 @@ BOOST_AUTO_TEST_CASE(linear_interpolation)
 
     std::function<double(double)> f_prime = interpolation.df;
     BOOST_CHECK_CLOSE(f_prime(50.0), 1e-2, 1e-10);
-}
-
-BOOST_AUTO_TEST_CASE(cubic_spline_interpolation)
-{
-    std::vector<std::array<double, 2>> values = {{{0.0, 0.0},
-                                                {100.0, 1.0}}};
-    auto interpolation = NuTo::Math::CubicSplineInterpolation(values);
-
-    BOOST_CHECK_CLOSE(interpolation(50.0), 0.5, 1e-10);
-
-    std::vector<double> x = {1.0, 3.0, 5.0, 7.0, 9.0}; 
-    std::vector<double> y = {1.0, 3.0, 5.0, 7.0, 9.0}; 
-    std::vector<std::array<double, 2>> values2;
-    values2.resize(x.size());
-    for (unsigned i = 0; i < x.size(); ++i)
-    {
-        values2[i][0] = x[i];
-        values2[i][1] = y[i];
-    }
-
-    auto interpolation2 = NuTo::Math::CubicSplineInterpolation(values2);
-    std::vector<double> output;
-    output.resize(x.size());
-    for (unsigned i = 0; i < x.size(); ++i)
-    {
-        output[i] = interpolation2(x[i]);
-    }
-    CHECK_CLOSE_COLLECTION(output, y, 1e-10);
 }
