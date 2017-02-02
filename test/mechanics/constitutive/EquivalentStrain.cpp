b9fe354ddfc52e629dfe15fdd3ad2a97011b833c
Christoph Pohl <christophpohl@gmx.com>
2017-02-02 17:48:39 +0100

    Refactor EngineeringInvariants into two unit tests

---
+++ test/mechanics/constitutive/EquivalentStrain.cpp
@@ -0,0 +1,77 @@
+#include "mechanics/constitutive/inputoutput/EquivalentStrain.h"
+#include "mechanics/sections/SectionEnum.h"
+#include <eigen3/Eigen/Dense>
+
+#define BOOST_TEST_MODULE EquivalentStrain
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+// necessary to build with clang when boost has been compiled by gcc
+std::string boost::unit_test::ut_detail::normalize_test_case_name(const_string name)
+{
+    return (name[0] == '&' ? std::string(name.begin()+1, name.size()-1) : std::string(name.begin(), name.size() ));
+}
+
+using namespace NuTo;
+
+//! param planeState Defaults to plane stress; for 1D/3D irrelevant
+template <int TDim>
+void CheckLocalEqStrainDerivativesMises(ePlaneState planeState = ePlaneState::PLANE_STRESS)
+{
+    constexpr int VDim = ConstitutiveIOBase::GetVoigtDim(TDim);
+
+    double nu = 0.25;
+    double k = 9.81;
+
+    std::vector<Eigen::Matrix<double, VDim, 1>> strainCases(VDim*2, Eigen::Matrix<double, VDim, 1>::Zero());
+
+    // define test cases with exactly one value =! 0
+    for (int i = 0; i < VDim; ++i)
+    {
+        strainCases[i     ][i] =   M_PI;
+        strainCases[i+VDim][i] = - M_PI;
+    }
+
+    // plus a random case
+    strainCases.push_back(Eigen::Matrix<double, VDim, 1>::Random());
+
+    double delta = 1.e-8;
+
+    // check derivatives for plane stress and plane strain
+    for (auto strainCase : strainCases)
+    {
+        EngineeringStrain<TDim> strain;
+        strain.AsVector() = strainCase;
+
+        EquivalentStrainModifiedMises<TDim> modMises(strain, k, nu, planeState);
+
+        double localEqStrain0 = modMises.Get();
+
+        ConstitutiveVector<VDim> tangent = modMises.GetDerivative();
+        ConstitutiveVector<VDim> tangent_CDF;
+
+        // calculate derivative numerically
+        for (int i = 0; i < VDim; ++i)
+        {
+            strain[i] += delta;
+            EquivalentStrainModifiedMises<TDim> modMises1(strain, k, nu, planeState);
+            tangent_CDF[i] = (modMises1.Get() - localEqStrain0) / delta;
+            strain[i] -= delta;
+        }
+
+        BOOST_CHECK_SMALL((tangent - tangent_CDF).cwiseAbs().maxCoeff(), 1.e-6);
+    }
+}
+
+
+BOOST_AUTO_TEST_CASE(CheckEqStrain)
+{
+        CheckLocalEqStrainDerivativesMises<1>();
+
+        CheckLocalEqStrainDerivativesMises<2>(ePlaneState::PLANE_STRAIN);
+
+        CheckLocalEqStrainDerivativesMises<2>(ePlaneState::PLANE_STRESS);
+
+        CheckLocalEqStrainDerivativesMises<3>();
+}
