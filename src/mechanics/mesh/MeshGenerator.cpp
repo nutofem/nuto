313266bcc20f4f64340a6ffb501d2dda9a057dc3
Thomas Titscher <thomas.titscher@googlemail.com>
2017-02-08 21:59:11 +0100

    Remove Eigen from MeshGenerator for compatibility

--- src/mechanics/mesh/MeshGenerator.cpp
+++ src/mechanics/mesh/MeshGenerator.cpp
@@ -52,7 +52,6 @@ std::vector<std::vector<int>> GetElementNodeIds2D(const std::vector<int>& rCorne
     {
         return {rCornerNodes};
     }
-
     case eShapeType::TRIANGLE2D:
     {
         std::vector<int> e1{rCornerNodes[0], rCornerNodes[1], rCornerNodes[2]};
@@ -68,29 +67,13 @@ std::vector<std::vector<int>> GetElementNodeIds2D(const std::vector<int>& rCorne
 namespace NuTo
 {
 
-template<>
-std::pair<int, int> MeshGenerator::Grid<1>(Structure& rS, std::array<double, 1> rEnd, std::array<int, 1> rNumDivisions)
-{
-    return MeshGenerator::Grid<1>(rS, rEnd, rNumDivisions, eShapeType::TRUSS1D);
-}
-template<>
-std::pair<int, int> MeshGenerator::Grid<2>(Structure& rS, std::array<double, 2> rEnd, std::array<int, 2> rNumDivisions)
-{
-    return MeshGenerator::Grid<2>(rS, rEnd, rNumDivisions, eShapeType::QUAD2D);
-}
-template<>
-std::pair<int, int> MeshGenerator::Grid<3>(Structure& rS, std::array<double, 3> rEnd, std::array<int, 3> rNumDivisions)
-{
-    return MeshGenerator::Grid<3>(rS, rEnd, rNumDivisions, eShapeType::BRICK3D);
-}
-
 
 
 template<>
-std::pair<int, int> MeshGenerator::CreateGrid<1>(Structure& rS,
-                                              Eigen::Matrix<double, 1, 1> rEnd,
-                                              Eigen::Matrix<int, 1, 1> rNumDivisions,
-                                              eShapeType rElementShape)
+std::pair<int, int> MeshGenerator::Grid<1>(Structure& rS,
+                                           std::array<double, 1> rEnd,
+                                           std::array<int, 1> rNumDivisions,
+                                           eShapeType rElementShape)
 {
     int numNodes = rNumDivisions[0] + 1;
     double delta = rEnd[0] / rNumDivisions[0];
@@ -115,33 +98,38 @@ std::pair<int, int> MeshGenerator::CreateGrid<1>(Structure& rS,
 }
 
 template<>
-std::pair<int, int> MeshGenerator::CreateGrid<2>(Structure& rS,
-                                              Eigen::Matrix<double, 2, 1> rEnd,
-                                              Eigen::Matrix<int, 2, 1> rNumDivisions,
-                                              Interpolation::eShapeType rElementShape)
+std::pair<int, int> MeshGenerator::Grid<2>(Structure& rS,
+                                           std::array<double, 2> rEnd,
+                                           std::array<int, 2> rNumDivisions,
+                                           Interpolation::eShapeType rElementShape)
 {
-    Eigen::Vector2i numNodes = rNumDivisions.array() + 1;
-    Eigen::Vector2d delta = rEnd.cwiseQuotient(rNumDivisions.cast<double>());
+    std::array<int, 2> numNodes;
+    std::array<double, 2> delta;
+
+    for (int i = 0; i < 2; ++i)
+    {
+        numNodes[i] = rNumDivisions[i] + 1;
+        delta[i] = rEnd[i] / rNumDivisions[i];
+    }
 
     std::vector<int> nodeIds;
-    nodeIds.reserve(numNodes.prod());
+    nodeIds.reserve(numNodes[0] * numNodes[1]);
 
-
-    for (int iY = 0; iY < numNodes.y(); ++iY)
-        for (int iX = 0; iX < numNodes.x(); ++iX)
-            nodeIds.push_back(rS.NodeCreate(Eigen::Vector2d(iX * delta.x(), iY * delta.y())));
+    for (int iY = 0; iY < numNodes[1]; ++iY)
+        for (int iX = 0; iX < numNodes[0]; ++iX)
+            nodeIds.push_back(rS.NodeCreate(Eigen::Vector2d(iX * delta[0], iY * delta[1])));
 
     auto info = CreateInterpolationTypeAndGroup(rS, rElementShape);
 
-    for (int iY = 0; iY < rNumDivisions.y(); ++iY)
-        for (int iX = 0; iX < rNumDivisions.x(); ++iX)
+    for (int iY = 0; iY < rNumDivisions[1]; ++iY)
+        for (int iX = 0; iX < rNumDivisions[0]; ++iX)
         {
             std::vector<int> cornerNodes(4);
 
-            cornerNodes[0] = nodeIds[iX   +  iY    * numNodes.x()];
-            cornerNodes[1] = nodeIds[iX+1 +  iY    * numNodes.x()];
-            cornerNodes[2] = nodeIds[iX+1 + (iY+1) * numNodes.x()];
-            cornerNodes[3] = nodeIds[iX   + (iY+1) * numNodes.x()];
+            cornerNodes[0] = nodeIds[iX   +  iY    * numNodes[0]];
+            cornerNodes[1] = nodeIds[iX+1 +  iY    * numNodes[0]];
+            cornerNodes[2] = nodeIds[iX+1 + (iY+1) * numNodes[0]];
+            cornerNodes[3] = nodeIds[iX   + (iY+1) * numNodes[0]];
 
             auto elementNodes = GetElementNodeIds2D(cornerNodes, rElementShape);
             for (auto& nodes : elementNodes)
@@ -151,39 +139,44 @@ std::pair<int, int> MeshGenerator::CreateGrid<2>(Structure& rS,
 }
 
 template<>
-std::pair<int, int> MeshGenerator::CreateGrid<3>(Structure& rS,
-                                              Eigen::Matrix<double, 3, 1> rEnd,
-                                              Eigen::Matrix<int, 3, 1> rNumDivisions,
-                                              eShapeType rElementShape)
+std::pair<int, int> MeshGenerator::Grid<3>(Structure& rS,
+                                           std::array<double, 3> rEnd,
+                                           std::array<int, 3> rNumDivisions,
+                                           eShapeType rElementShape)
 {
-    Eigen::Vector3i numNodes = rNumDivisions.array() + 1;
-    Eigen::Vector3d delta = rEnd.cwiseQuotient(rNumDivisions.cast<double>());
+    std::array<int, 3> numNodes;
+    std::array<double, 3> delta;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        numNodes[i] = rNumDivisions[i] + 1;
+        delta[i] = rEnd[i] / rNumDivisions[i];
+    }
 
     std::vector<int> nodeIds;
-    nodeIds.reserve(numNodes.prod());
+    nodeIds.reserve(numNodes[0] * numNodes[1] * numNodes[2]);
 
-
-    for (int iZ = 0; iZ < numNodes.z(); ++iZ)
-        for (int iY = 0; iY < numNodes.y(); ++iY)
-            for (int iX = 0; iX < numNodes.x(); ++iX)
-                nodeIds.push_back(rS.NodeCreate(Eigen::Vector3d(iX * delta.x(), iY * delta.y(), iZ * delta.z())));
+    for (int iZ = 0; iZ < numNodes[2]; ++iZ)
+        for (int iY = 0; iY < numNodes[1]; ++iY)
+            for (int iX = 0; iX < numNodes[0]; ++iX)
+                nodeIds.push_back(rS.NodeCreate(Eigen::Vector3d(iX * delta[0], iY * delta[1], iZ * delta[2])));
 
     auto info = CreateInterpolationTypeAndGroup(rS, rElementShape);
 
-    for (int iZ = 0; iZ < rNumDivisions.z(); ++iZ)
-        for (int iY = 0; iY < rNumDivisions.y(); ++iY)
-            for (int iX = 0; iX < rNumDivisions.x(); ++iX)
+    for (int iZ = 0; iZ < rNumDivisions[2]; ++iZ)
+        for (int iY = 0; iY < rNumDivisions[1]; ++iY)
+            for (int iX = 0; iX < rNumDivisions[0]; ++iX)
             {
                 std::vector<int> cornerNodes(8);
 
-                cornerNodes[0] = nodeIds[iX   +  iY    * numNodes.x() +  iZ    * numNodes.x() * numNodes.y()];
-                cornerNodes[1] = nodeIds[iX+1 +  iY    * numNodes.x() +  iZ    * numNodes.x() * numNodes.y()];
-                cornerNodes[2] = nodeIds[iX+1 + (iY+1) * numNodes.x() +  iZ    * numNodes.x() * numNodes.y()];
-                cornerNodes[3] = nodeIds[iX   + (iY+1) * numNodes.x() +  iZ    * numNodes.x() * numNodes.y()];
-                cornerNodes[4] = nodeIds[iX   +  iY    * numNodes.x() + (iZ+1) * numNodes.x() * numNodes.y()];
-                cornerNodes[5] = nodeIds[iX+1 +  iY    * numNodes.x() + (iZ+1) * numNodes.x() * numNodes.y()];
-                cornerNodes[6] = nodeIds[iX+1 + (iY+1) * numNodes.x() + (iZ+1) * numNodes.x() * numNodes.y()];
-                cornerNodes[7] = nodeIds[iX   + (iY+1) * numNodes.x() + (iZ+1) * numNodes.x() * numNodes.y()];
+                cornerNodes[0] = nodeIds[iX   +  iY    * numNodes[0] +  iZ    * numNodes[0] * numNodes[1]];
+                cornerNodes[1] = nodeIds[iX+1 +  iY    * numNodes[0] +  iZ    * numNodes[0] * numNodes[1]];
+                cornerNodes[2] = nodeIds[iX+1 + (iY+1) * numNodes[0] +  iZ    * numNodes[0] * numNodes[1]];
+                cornerNodes[3] = nodeIds[iX   + (iY+1) * numNodes[0] +  iZ    * numNodes[0] * numNodes[1]];
+                cornerNodes[4] = nodeIds[iX   +  iY    * numNodes[0] + (iZ+1) * numNodes[0] * numNodes[1]];
+                cornerNodes[5] = nodeIds[iX+1 +  iY    * numNodes[0] + (iZ+1) * numNodes[0] * numNodes[1]];
+                cornerNodes[6] = nodeIds[iX+1 + (iY+1) * numNodes[0] + (iZ+1) * numNodes[0] * numNodes[1]];
+                cornerNodes[7] = nodeIds[iX   + (iY+1) * numNodes[0] + (iZ+1) * numNodes[0] * numNodes[1]];
 
                 auto elementNodes = GetElementNodeIds3D(cornerNodes, rElementShape);
                 for (auto& nodes : elementNodes)
@@ -192,6 +185,23 @@ std::pair<int, int> MeshGenerator::CreateGrid<3>(Structure& rS,
 
     return info;
 }
+
+template<>
+std::pair<int, int> MeshGenerator::Grid<1>(Structure& rS, std::array<double, 1> rEnd, std::array<int, 1> rNumDivisions)
+{
+    return MeshGenerator::Grid<1>(rS, rEnd, rNumDivisions, eShapeType::TRUSS1D);
+}
+template<>
+std::pair<int, int> MeshGenerator::Grid<2>(Structure& rS, std::array<double, 2> rEnd, std::array<int, 2> rNumDivisions)
+{
+    return MeshGenerator::Grid<2>(rS, rEnd, rNumDivisions, eShapeType::QUAD2D);
+}
+template<>
+std::pair<int, int> MeshGenerator::Grid<3>(Structure& rS, std::array<double, 3> rEnd, std::array<int, 3> rNumDivisions)
+{
+    return MeshGenerator::Grid<3>(rS, rEnd, rNumDivisions, eShapeType::BRICK3D);
+}
+
 
 } // namespace NuTo
 
--- src/mechanics/mesh/MeshGenerator.h
+++ src/mechanics/mesh/MeshGenerator.h
@@ -3,7 +3,6 @@
 
 #include <array>
 #include <functional>
-#include <vector>
 #include <eigen3/Eigen/Dense>
 #include "mechanics/interpolationtypes/InterpolationTypeEnum.h"
 
@@ -25,19 +24,7 @@ public:
     static std::pair<int, int> Grid(Structure& rS,
                                     std::array<double, TDim> rEnd,
                                     std::array<int, TDim> rNumDivisions,
-                                    Interpolation::eShapeType rElementShape)
-    {
-        Eigen::Matrix<double, TDim, 1> end;
-        Eigen::Matrix<int, TDim, 1> divs;
-
-        for (int i = 0; i < TDim; ++i)
-        {
-            end[i] = rEnd[i];
-            divs[i] = rNumDivisions[i];
-        }
-        return CreateGrid(rS, end, divs, rElementShape);
-    }
-
+                                    Interpolation::eShapeType rElementShape);
 
     //! @brief creates a grid from origin to rEnd with a default element shape
     //!        1D = Truss | 2D = Quad | 3D = Brick
@@ -53,20 +40,5 @@ public:
     //! @param rRadius ... radius of the cylinder
     //! @param rHeight ... height of the cylinder
     static std::function<Eigen::Vector3d(Eigen::Vector3d)> GetCylinderMapping(double rRadius, double rHeight);
-
-
-private:
-
-    //! @brief creates a grid from origin to rEnd
-    //! @param rS structure
-    //! @param rEnd coordinates of the end point
-    //! @param rNumDivisions number of grid cells in each direction
-    //! @param rElementShape element shape (truss, quad, triangle, ...)
-    //! @return pair<groupId, interpolationTypeId>
-    template <int TDim>
-    static std::pair<int, int> CreateGrid(Structure& rS,
-                                          Eigen::Matrix<double, TDim, 1> rEnd,
-                                          Eigen::Matrix<int, TDim, 1> rNumDivisions,
-                                          Interpolation::eShapeType rElementShape);
 };
 }
