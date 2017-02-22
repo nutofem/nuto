9e4b7e19b6d23c1958685e5561ffe09a94c249e0
Christoph Pohl <christophpohl@gmx.com>
2017-02-22 19:08:10 +0100

    More output hiding

--- scripts/upload_coverage.sh
+++ scripts/upload_coverage.sh
@@ -2,9 +2,9 @@
 set -ev
 if [[ "$COVERAGE" == "TRUE" ]]; then
     # get coverage information
-    lcov --capture --directory build --output-file coverage.info > /dev/null
+    lcov --capture --directory build --output-file coverage.info 2> /dev/null
     # filter out system stuff that we don't control
     lcov -r coverage.info '/usr/include/*' -o coverage.info > /dev/null
     # upload to codecov
-    bash <(curl -s https://codecov.io/bash)
+    bash <(curl -s https://codecov.io/bash) > /dev/null 2> /dev/null
 fi
