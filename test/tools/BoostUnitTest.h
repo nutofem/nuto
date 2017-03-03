18b7202f43dea63aa55920d5b6bb37c3229fc884
Thomas Titscher <thomas.titscher@gmail.com>
2017-03-03 12:22:28 +0100

    Add method to check eigen matrices

--- test/tools/BoostUnitTest.h
+++ test/tools/BoostUnitTest.h
@@ -21,4 +21,19 @@ void CheckVector(const T1& r1, const T2& r2, int rSize, double rTolerance = 1.e-
     for (int i = 0; i < rSize; ++i)
         BOOST_CHECK_CLOSE(r1[i], r2[i], rTolerance);
 }
+
+template <typename T1, typename T2>
+void CheckEigenMatrix(const T1& r1, const T2& r2, double rTolerance = 1.e-10)
+{
+    BOOST_CHECK_EQUAL(r1.rows(), r2.rows());
+    BOOST_CHECK_EQUAL(r1.cols(), r2.cols());
+
+    bool isApprox = r1.isApprox(r2);
+    if (not isApprox)
+    {
+        BOOST_TEST_MESSAGE("1st matrix: \n" << r1);
+        BOOST_TEST_MESSAGE("2nd matrix: \n" << r2);
+    }
+    BOOST_CHECK(isApprox);
+}
 } /* BoostUnitTest */
