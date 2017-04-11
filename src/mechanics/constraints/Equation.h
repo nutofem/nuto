cb369def703922edff17504c1aa79d536fa572c5
Thomas Titscher <thomas.titscher@gmail.com>
2017-04-11 13:54:30 +0200

    Add documentation for Term, Equation, Constraints
    [ci skip]

--- src/mechanics/constraints/Equation.h
+++ src/mechanics/constraints/Equation.h
@@ -6,66 +6,77 @@ namespace NuTo
 {
 namespace Constraint
 {
-
 typedef std::function<double(double)> RhsFunction;
 
+//! @brief stores a constraint equation
 class Equation
 {
 public:
+    //! @brief ctor with constant rhs, defaults to 0
+    //! @param rhs value for the constant rhs
     Equation(double rhs = 0)
         : mRhs([=](double) { return rhs; })
     {
     }
 
+    //! @brief ctor with terms and constant rhs
+    //! @param terms equation terms
+    //! @param rhs value for the constant rhs
     Equation(std::vector<Term> terms, double rhs = 0)
         : mRhs([=](double) { return rhs; })
         , mTerms(terms)
     {
     }
 
+    //! @brief ctor with a rhs function
+    //! @param rhs rhs function
     Equation(RhsFunction rhs)
         : mRhs(rhs)
     {
     }
 
+    //! @brief ctor with a rhs function and terms
+    //! @param rhs rhs function
+    //! @param terms equation terms
     Equation(RhsFunction rhs, std::vector<Term> terms)
         : mRhs(rhs)
         , mTerms(terms)
     {
     }
 
-#ifndef SWIG
-    Equation(const Equation&) = default;
-    Equation(Equation&&) = default;
-
-    Equation& operator=(const Equation&) = default;
-    Equation& operator=(Equation&&) = default;
-#endif /* SWIG */
-
+    //! @brief adds a term to the equation
+    //! @param term equation term
     void AddTerm(Term term)
     {
         mTerms.push_back(term);
     }
 
-    const std::vector<Term>& GetTerms() const
-    {
-        return mTerms;
-    }
-
-    std::vector<Term>& GetTerms()
-    {
-        return mTerms;
-    }
-
+    //! @brief evaluates the rhs function at a given time
+    //! @param time global time
+    //! @return rhs(time)
     double GetRhs(double time) const
     {
         return mRhs(time);
     }
 
-    ~Equation() = default;
+    //! @brief getter for mTerms
+    const std::vector<Term>& GetTerms() const
+    {
+        return mTerms;
+    }
+
+    //! @brief nonconst getter for mTerms
+    //! @remark IMO only used for ExchangeNodePtr which is very questionable...
+    std::vector<Term>& GetTerms()
+    {
+        return mTerms;
+    }
 
 private:
+    //! @brief rhs function
     RhsFunction mRhs;
+
+    //! @brief terms
     std::vector<Term> mTerms;
 };
 
