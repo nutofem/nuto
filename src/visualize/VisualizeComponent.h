a1a3b9f44b58e9c2dfa8dd7ee4b8d64daa02f671
Thomas Titscher <thomas.titscher@bam.de>
2017-04-23 11:23:15 +0200

    Add VisualizeComponent [ci skip]

---
+++ src/visualize/VisualizeComponent.h
@@ -0,0 +1,46 @@
+// $Id$ 
+#pragma once
+#ifdef ENABLE_SERIALIZATION
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/export.hpp>
+#endif // ENABLE_SERIALIZATION
+
+#include <string>
+#include "visualize/VisualizeEnum.h"
+
+namespace NuTo
+{
+
+class VisualizeComponent
+{
+#ifdef ENABLE_SERIALIZATION
+    friend class boost::serialization::access;
+    
+    //! @brief just for serialization
+    VisualizeComponent() = default;
+#endif // ENABLE_SERIALIZATION
+public:
+    VisualizeComponent(NuTo::eVisualizeWhat visualizeComponent);
+
+    NuTo::eVisualizeWhat GetComponentEnum() const;
+
+    std::string GetComponentName() const;
+
+#ifdef ENABLE_SERIALIZATION
+    //! @brief serializes the class
+    //! @param ar         archive
+    //! @param version    version
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version);
+#endif // ENABLE_SERIALIZATION
+
+private:
+    eVisualizeWhat mVisualizeComponent;
+
+};
+}
+
+#ifdef ENABLE_SERIALIZATION
+BOOST_CLASS_EXPORT_KEY(NuTo::VisualizeComponent)
+#endif // ENABLE_SERIALIZATION
+
