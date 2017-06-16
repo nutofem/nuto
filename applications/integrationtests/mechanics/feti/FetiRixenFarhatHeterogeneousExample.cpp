ad7aec4cf24ab0d271ca2d999a5bd8c38288b6bb
Philip Huschke <philiphuschke@gmail.com>
2017-06-16 11:38:08 +0200

    Add new feti test with 16 subdomains

--- applications/integrationtests/mechanics/feti/FetiRixenFarhatHeterogeneousExample.cpp
+++ applications/integrationtests/mechanics/feti/FetiRixenFarhatHeterogeneousExample.cpp
@@ -13,6 +13,7 @@
 
 #include "visualize/VisualizeEnum.h"
 
+
 using std::cout;
 using std::endl;
 using NuTo::Constitutive::ePhaseFieldEnergyDecomposition;
@@ -33,26 +34,28 @@ using FetiPreconditioner = NuTo::NewmarkFeti<EigenSolver>::eFetiPreconditioner;
 using FetiIterativeSolver = NuTo::NewmarkFeti<EigenSolver>::eIterativeSolver;
 using FetiScaling = NuTo::NewmarkFeti<EigenSolver>::eFetiScaling;
 
+using EigenSolver = Eigen::SparseLU<Eigen::SparseMatrix<double>, Eigen::COLAMDOrdering<int>>;
 
 // geometry
 constexpr int dim = 2;
+constexpr double lengthX = 20.;
 constexpr double thickness = 1.0;
-constexpr double lengthX = 40.;
 
 // material
-constexpr double youngsModulus = 2.1e5;
-constexpr double poissonsRatio = 0.0;
+double youngsModulus = 4094;
+constexpr double poissonsRatio = 0.3;
 
 // integration
-constexpr double timeStep = 1.;
-constexpr double simulationTime = 1.;
-constexpr double toleranceDisp = 1e-6;
-constexpr double loadFactor = 10.0;
-
-// auxiliary
+constexpr bool performLineSearch = false;
+constexpr bool automaticTimeStepping = false;
+constexpr double timeStep = 1e-0;
+constexpr double minTimeStep = 1e-1;
+constexpr double maxTimeStep = 1e-0;
+constexpr double toleranceDisp = 1e-5;
+constexpr double simulationTime = 1.0;
+constexpr double loadFactor = 1.0;
+constexpr double maxIterations = 1;
 constexpr double tol = 1.e-6;
-const Vector2d directionX = Vector2d::UnitX();
-const Vector2d directionY = Vector2d::UnitY();
 
 int main(int argc, char* argv[])
 {
@@ -63,48 +66,52 @@ int main(int argc, char* argv[])
 
     NuTo::StructureFeti structure(dim);
     structure.SetNumTimeDerivatives(0);
-    structure.SetVerboseLevel(10);
-    structure.SetShowTime(true);
+    structure.SetVerboseLevel(5);
+    structure.SetShowTime(false);
     structure.GetLogger().OpenFile("output" + std::to_string(rank));
     structure.GetLogger().SetQuiet(true);
 
-    std::string meshFile = "FetiMultiplicityScaling.mesh" + std::to_string(rank);
+    std::string meshFile = "FetiRixenFarhatHeterogeneousExample.mesh" + std::to_string(rank);
 
     const int interpolationTypeId = structure.InterpolationTypeCreate(eShapeType::QUAD2D);
-    structure.InterpolationTypeAdd(interpolationTypeId, eDof::DISPLACEMENTS, eTypeOrder::EQUIDISTANT1);
     structure.InterpolationTypeAdd(interpolationTypeId, eDof::COORDINATES, eTypeOrder::EQUIDISTANT1);
+    structure.InterpolationTypeAdd(interpolationTypeId, eDof::DISPLACEMENTS, eTypeOrder::EQUIDISTANT1);
 
     structure.ImportMeshJson(meshFile, interpolationTypeId);
 
-    structure.GetLogger() << "*********************************** \n"
-                          << "**      material                 ** \n"
-                          << "*********************************** \n\n";
+    if (rank == 8 or rank == 9 or rank == 10 or rank == 11)
+    {
+        youngsModulus = 1;
+    }
+
 
     const int materialId = structure.ConstitutiveLawCreate(eConstitutiveType::LINEAR_ELASTIC_ENGINEERING_STRESS);
     structure.ConstitutiveLawSetParameterDouble(materialId, eConstitutiveParameter::YOUNGS_MODULUS, youngsModulus);
     structure.ConstitutiveLawSetParameterDouble(materialId, eConstitutiveParameter::POISSONS_RATIO, poissonsRatio);
     structure.ElementTotalSetConstitutiveLaw(materialId);
 
-    structure.GetLogger() << "*********************************** \n"
-                          << "**      section                  ** \n"
-                          << "*********************************** \n\n";
 
     auto section = NuTo::SectionPlane::Create(thickness, true);
     structure.ElementTotalSetSection(section);
 
+    structure.ExportVtkDataFileElements("elements_" + std::to_string(rank) + ".vtk");
+
     structure.GetLogger() << "*********************************** \n"
-                          << "**      node groups              ** \n"
+                          << "**      create node groups       ** \n"
                           << "*********************************** \n\n";
 
     const auto& groupNodesLeftBoundary = structure.GroupGetNodesAtCoordinate(eDirection::X, 0.);
     const auto& groupNodesLoad = structure.GroupGetNodesAtCoordinate(eDirection::X, lengthX);
 
+
     structure.GetLogger() << "*********************************** \n"
                           << "**      virtual constraints      ** \n"
                           << "*********************************** \n\n";
 
+
     std::vector<int> nodeIdsBoundaries = groupNodesLeftBoundary.GetMemberIds();
     std::vector<int> nodeIdsLoads = groupNodesLoad.GetMemberIds();
+
 
     structure.ApplyVirtualConstraints(nodeIdsBoundaries, nodeIdsLoads);
 
@@ -112,25 +119,38 @@ int main(int argc, char* argv[])
                           << "**      real constraints         ** \n"
                           << "*********************************** \n\n";
 
+
     structure.NodeBuildGlobalDofs(__PRETTY_FUNCTION__);
 
-    structure.ApplyConstraintsTotalFeti(groupNodesLeftBoundary);
+    structure.NodeInfo(10);
+
+    std::vector<int> boundaryDofIds;
+    for (const int nodeId : nodeIdsBoundaries)
+    {
+        std::vector<int> dofIds = structure.NodeGetDofIds(nodeId, eDof::DISPLACEMENTS);
+        boundaryDofIds.push_back(dofIds[0]);
+        boundaryDofIds.push_back(dofIds[1]);
+    }
+
+    structure.ApplyConstraintsTotalFeti(boundaryDofIds);
+
 
     structure.GetLogger() << "*********************************** \n"
                           << "**      load                     ** \n"
                           << "*********************************** \n\n";
 
-    // prescribe displacement of groupNodesLoad in Y direction
+
     std::map<int, double> dofIdAndPrescribedDisplacementMap;
+
     for (auto const& nodeId : nodeIdsLoads)
     {
         std::vector<int> dofIds = structure.NodeGetDofIds(nodeId, eDof::DISPLACEMENTS);
-        dofIdAndPrescribedDisplacementMap.emplace(dofIds[0], 1.);
+        dofIdAndPrescribedDisplacementMap.emplace(dofIds[0], 0.);
     }
 
     structure.ApplyPrescribedDisplacements(dofIdAndPrescribedDisplacementMap);
 
-    int loadId = structure.LoadCreateNodeGroupForce(&groupNodesLoad, directionX, 0.);
+    int loadId = structure.LoadCreateNodeGroupForce(&groupNodesLoad, Vector2d::UnitY(), 0.);
 
 
     structure.GetLogger() << "*********************************** \n"
@@ -141,24 +161,31 @@ int main(int argc, char* argv[])
     structure.GroupCreate(groupAllElements, eGroupId::Elements);
     structure.GroupAddElementsTotal(groupAllElements);
     structure.AddVisualizationComponent(groupAllElements, eVisualizeWhat::DISPLACEMENTS);
+    structure.AddVisualizationComponent(groupAllElements, eVisualizeWhat::ENGINEERING_STRESS);
+
 
     structure.GetLogger() << "*********************************** \n"
                           << "**      integration scheme       ** \n"
                           << "*********************************** \n\n";
 
-
     NuTo::NewmarkFeti<EigenSolver> newmarkFeti(&structure);
-
-    boost::filesystem::path resultPath(boost::filesystem::initial_path().string() + "/feti_" + std::to_string(rank));
+    boost::filesystem::path resultPath(boost::filesystem::path(getenv("HOME")).string() +
+                                       std::string("/results/feti/linear_elastic/rixen_heterogeneous_example/") +
+                                       std::to_string(structure.mRank));
 
     newmarkFeti.SetTimeStep(timeStep);
-    newmarkFeti.SetResultDirectory(resultPath.string(), true);
-    newmarkFeti.SetToleranceIterativeSolver(1.e-8);
-    newmarkFeti.SetMaxNumberOfFetiIterations(100);
 
-    newmarkFeti.SetIterativeSolver(FetiIterativeSolver::ConjugateGradient);
-    newmarkFeti.SetFetiPreconditioner(FetiPreconditioner::Lumped);
-    newmarkFeti.SetFetiScaling(FetiScaling::None);
+    newmarkFeti.SetMinTimeStep(minTimeStep);
+    newmarkFeti.SetMaxTimeStep(maxTimeStep);
+    newmarkFeti.SetMaxNumIterations(maxIterations);
+    newmarkFeti.SetAutomaticTimeStepping(automaticTimeStepping);
+    newmarkFeti.SetResultDirectory(resultPath.string(), true);
+    newmarkFeti.SetPerformLineSearch(performLineSearch);
+    newmarkFeti.SetToleranceResidual(eDof::DISPLACEMENTS, toleranceDisp);
+    newmarkFeti.SetIterativeSolver(NuTo::NewmarkFeti<EigenSolver>::eIterativeSolver::ConjugateGradient);
+    newmarkFeti.SetFetiPreconditioner(NuTo::NewmarkFeti<EigenSolver>::eFetiPreconditioner::Lumped);
+    newmarkFeti.SetMaxNumberOfFetiIterations(1000);
+    newmarkFeti.SetToleranceIterativeSolver(1.e-6);
 
     Eigen::Matrix2d dispRHS;
     dispRHS(0, 0) = 0;
@@ -172,5 +199,7 @@ int main(int argc, char* argv[])
                           << "**      solve                    ** \n"
                           << "*********************************** \n\n";
 
+
     newmarkFeti.Solve(simulationTime);
+    structure.GetLogger() << "Total number of Dofs: \t" << structure.GetNumTotalDofs() << "\n\n";
 }
