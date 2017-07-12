c75566a9f9b2b32e49ea69b8c7961eda5dbd1c0d
Christoph Pohl <christophpohl@gmx.com>
2017-07-12 17:45:20 +0200

    Fix missing python examples

--- applications/integrationtests/mechanics/ElectricWave.py
+++ applications/integrationtests/mechanics/ElectricWave.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 import unittest
 import nuto
-import sys
 import numpy as np
+
 
 class Wave1D(unittest.TestCase):
     def setUp(self):
         self.s = nuto.Structure(1)
         self.s.SetNumTimeDerivatives(2)
         self.s.SetShowTime(False)
-        self.s.SetVerboseLevel(7) # sounds about right 
+        self.s.SetVerboseLevel(7)  # sounds about right
 
         mesh_info = nuto.MeshGenerator_Grid(self.s, [2.], [25])
         self.s.InterpolationTypeAdd(mesh_info[1], "ElectricPotential", "Lobatto4")
@@ -23,21 +23,21 @@ class Wave1D(unittest.TestCase):
         self.s.AddVisualizationComponent(self.s.GroupGetElementsTotal(), "ElectricPotential")
 
         n = self.s.NodeGetAtCoordinate(0)
-        self.s.Constraints().Add(nuto.eDof_ELECTRICPOTENTIAL, nuto.Value(n, self.load)) 
+        self.s.Constraints().Add(nuto.eDof_ELECTRICPOTENTIAL, nuto.Value(n, self.load))
 
         self.rk4 = nuto.RungeKutta4(self.s)
         self.rk4.SetTimeStep(0.01)
         self.rk4.SetShowTime(False)
 
     def load(self, time):
-        return np.sin(time * 2 * np.pi )
+        return np.sin(time * 2 * np.pi)
 
     def test_time_step(self):
         self.assertLess(self.rk4.GetTimeStep(), self.rk4.CalculateCriticalTimeStep())
 
     def test_check_solution(self):
-        self.rk4.SetResultDirectory("./ElectricWave/", True)
-        self.rk4.Solve(2 - 1.e-6) # without 1.e-6, it would end at t = 2.0049999
+        self.rk4.PostProcessing().SetResultDirectory("./ElectricWave/", True)
+        self.rk4.Solve(2 - 1.e-6)  # without 1.e-6, it would end at t = 2.0049999
         nodes = self.s.GroupCreate("Nodes")
         self.s.GroupAddNodeCoordinateRange(nodes, 0, 0, 1.)
         for node_id in self.s.GroupGetMemberIds(nodes):
@@ -45,7 +45,8 @@ class Wave1D(unittest.TestCase):
             x = node.Get(nuto.eDof_COORDINATES)[0][0]
             potential = node.Get(nuto.eDof_ELECTRICPOTENTIAL)[0][0]
             analytic = -np.sin(2 * np.pi * x)
-            self.assertAlmostEqual(potential, analytic, 2) # correct up to 3 decimal places
+            self.assertAlmostEqual(potential, analytic, 2)  # correct up to 3 decimal places
+
 
 if __name__ == '__main__':
     unittest.main()
