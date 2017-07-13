43fc1831551435df141de11fd1631118fb11c926
Philip Huschke <philiphuschke@gmail.com>
2017-07-13 17:48:11 +0200

    Removed -Wextra flag (see #21)
    [ci skip]

--- scripts/CompilerFlags.cmake
+++ scripts/CompilerFlags.cmake
@@ -1,5 +1,5 @@
 # use newer c++ standard, enable additional warnings, support for sse4
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra -Wall -msse4 -pedantic -std=c++14 -fPIC")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -msse4 -pedantic -std=c++14 -fPIC")
 
 # find openmp
 if(ENABLE_OPENMP)
