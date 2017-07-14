c49cf3ae41665933760ca218f5c6e349aca553d6
Christoph Pohl <christophpohl@gmx.com>
2017-07-14 13:36:32 +0200

    Replace git parsing with foreign solution
    This is more robust in detecting new commits than my naive
    implementation was.

--- scripts/Version.cmake
+++
@@ -1,8 +0,0 @@
-# save output from `git rev-parse HEAD` to variable `commit_nr`
-execute_process(COMMAND
-    git rev-parse HEAD
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    OUTPUT_VARIABLE commit_nr)
-
-# remove trailing newline
-string(REGEX REPLACE "\n$" "" commit_nr "${commit_nr}")
