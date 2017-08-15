a35ce7c84c5ee4bdb10b30de32fec9636e2fc093
Christoph Pohl <christophpohl@gmx.com>
2017-08-15 17:32:18 +0200

    Add create_nuto_module function to CMake

--- scripts/NuToMacros.cmake
+++ scripts/NuToMacros.cmake
@@ -95,3 +95,21 @@ endfunction()
 function(create_symlink pathName)
     execute_process(COMMAND "${CMAKE_COMMAND}" "-E" "create_symlink" "${CMAKE_CURRENT_SOURCE_DIR}/${pathName}" "${CMAKE_CURRENT_BINARY_DIR}/${pathName}")
 endfunction()
+
+function(create_nuto_module ModuleName ModuleSources)
+    sources_to_objects("${ModuleSources}" ModuleObjects)
+
+    add_library(${ModuleName} ${ModuleObjects} ${ARGN})
+    set_target_properties(${ModuleName} PROPERTIES
+        OUTPUT_NAME NuTo${ModuleName}
+        )
+    target_include_directories(${ModuleName} PUBLIC
+        $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/src>
+        $<INSTALL_INTERFACE:include/nuto>
+        )
+
+    install(TARGETS ${ModuleName} EXPORT NuToTargets
+        LIBRARY DESTINATION lib
+        INCLUDES DESTINATION include/nuto
+        )
+endfunction()
