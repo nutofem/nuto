39f6ea498e05e2124e43317757c142b8549ba1d5
Thomas Titscher <thomas.titscher@gmail.com>
2017-09-21 17:53:44 +0200

    Add GetNodeValues() cache

--- src/mechanics/cell/CellData.h
+++ src/mechanics/cell/CellData.h
@@ -10,7 +10,7 @@ namespace NuTo
 //! @brief Extracts 'cell data' like nodal values from the cell
 //! @remark The life time of objects of this class is limited to the evaluation of one NuTo::Cell. This class will
 //! calculate (and cache) all the information that are required once per cell. Classic example: NodeValues. Even if
-//! `GetNodeValues()` is called 42 times, they should only be extracted once. Ehrm [TODO]!
+//! `GetNodeValues()` is called 42 times, they should only be extracted once.
 class CellData
 {
 public:
@@ -21,10 +21,19 @@ public:
 
     NodeValues GetNodeValues(const DofType& dofType) const
     {
-        return mElements[dofType]->ExtractNodeValues();
+        NodeValues& nodeValues = mNodeValues[dofType];
+        if (nodeValues.size() == 0)
+        {
+#ifdef _OPENMP
+#pragma omp critical
+#endif
+            nodeValues = mElements[dofType]->ExtractNodeValues();
+        }
+        return nodeValues;
     }
 
 private:
+    mutable DofContainer<NodeValues> mNodeValues;
     const DofContainer<CellInterpolationBase*>& mElements;
 };
 } /* NuTo */
