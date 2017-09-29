f74db643d04450b94f4e65388be61ef8cb7488f1
Thomas Titscher <thomas.titscher@gmail.com>
2017-09-29 14:36:10 +0200

    Rework new Group to chronological ordering

--- src/base/Group.h
+++ src/base/Group.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <algorithm>
 #include <vector>
+#include <set>
 #include <boost/iterator/indirect_iterator.hpp>
 
 namespace NuTo
@@ -9,39 +9,22 @@ namespace NuTo
 namespace Groups
 {
 
-template <typename T>
-struct IdSmaller
-{
-    bool operator()(const T* a, const T* b) const
-    {
-        return a->Id() < b->Id();
-    }
-};
-
-class Utils;
-
 //! @brief Ordered container class for elements, nodes and the like. No duplicates.
 //!
 //! The group saves pointers to its members but can be iterated through
-//! by value. By default the members are sorted by id (this must be unique)
-//! that is accessed by calling Id(), e.g as provided by NuTo::UniqueId. Other
-//! methods operating on the group also use this comparator.
-//! This behaviour can be changed by providing a different comparator but the basis
-//! of the comparison has to be a unique attribute of the group member.
-//! See test file (CutomCompare).
+//! by value. The members are sorted in order of their insertion.
 //!
 //! @tparam T type of group members
 //! @tparam TCompare functor representing the comparison operation used for sorting
-template <typename T, typename TCompare = IdSmaller<T>>
+template <typename T>
 class Group : private std::vector<T*>
 {
-    friend class std::insert_iterator<Group>;
     typedef std::vector<T*> parent;
-
 public:
 
     //! @brief indirect (dereferencing) iterator to provide value semantics for the iterators
     typedef boost::indirect_iterator<typename parent::iterator> GroupIterator;
+    typedef boost::indirect_iterator<typename parent::const_iterator> ConstGroupIterator;
 
     //! @brief Create an empty group
     Group() = default;
@@ -52,7 +35,7 @@ public:
     {
         Add(element);
     }
-
+    
     //! @brief Create a group containing multiple elements
     //! @param elements vector of elements
     //! @remark std::reference_wrapper for value semantics
@@ -66,25 +49,16 @@ public:
     //! @param element single element to add
     void Add(T& element)
     {
-        auto it = std::lower_bound(pcbegin(), pcend(), &element, TCompare());
-        if (it == pcend() || TCompare()(&element, *it))
-            parent::insert(it, &element);
+        auto it = mUniqueData.insert(&element);
+        if (it.second)
+            parent::push_back(&element);
     }
 
     //! @brief True if element is contained in group
     //! @param element single element to check
     bool Contains(const T& element) const
     {
-        auto it = std::lower_bound(pcbegin(), pcend(), &element, TCompare());
-        if (it == pcend())
-            return false;
-        // *it is _not less_ than &element. = !comp(*it, &element) == true
-        // So *it can either be equal (-->return true) or greater(-->return false) 
-        //
-        // Equality in the compare concept is defined as:
-        // equal(*it, &element) == !comp(*it, &element) && !comp(&element, *it)
-        // Based on std::lower_bound, we already know that the first part is true. Check for the second.
-        return !TCompare()(&element, *it);
+        return mUniqueData.find(&element) != mUniqueData.end();
     }
 
     //! @brief True if group is empty
@@ -100,6 +74,18 @@ public:
     }
 
     //! @brief Iterate over group elements (by value not pointer)
+    ConstGroupIterator begin() const
+    {
+        return parent::begin();
+    }
+
+    //! @brief Iterate over group elements (by value not pointer)
+    ConstGroupIterator end() const
+    {
+        return parent::end();
+    }
+
+    //! @brief Iterate over group elements (by value not pointer)
     GroupIterator begin()
     {
         return parent::begin();
@@ -111,64 +97,48 @@ public:
         return parent::end();
     }
 
-    typename parent::iterator pbegin()
-    {
-        return parent::begin();
-    }
-
-    typename parent::const_iterator pcbegin() const
-    {
-        return parent::cbegin();
-    }
-
-    typename parent::const_iterator pcend() const
-    {
-        return parent::cend();
-    }
-
 private:
     using parent::size;
+    std::set<const T*> mUniqueData;
 };
 
 //! @brief Unite two groups
-template <typename T, typename TCompare>
-static Group<T, TCompare> Unite(const Group<T, TCompare>& one, const Group<T, TCompare>& two)
+template <typename T>
+static Group<T> Unite(const Group<T>& one, const Group<T>& two)
 {
-    Group<T, TCompare> newGroup;
-    std::insert_iterator<Group<T, TCompare>> newGroupInsertIterator(newGroup, newGroup.pbegin());
-    std::set_union(one.pcbegin(), one.pcend(), two.pcbegin(), two.pcend(), newGroupInsertIterator, TCompare());
+    Group<T> newGroup = one;
+    for (auto& t : two)
+        newGroup.Add(t);
     return newGroup;
 }
 
 //! @brief Returns group with elements of group one that are not in group two
-template <typename T, typename TCompare>
-static Group<T, TCompare> Difference(const Group<T, TCompare>& one, const Group<T, TCompare>& two)
+template <typename T>
+static Group<T> Difference(const Group<T>& one, const Group<T>& two)
 {
-    Group<T, TCompare> newGroup;
-    std::insert_iterator<Group<T, TCompare>> newGroupInsertIterator(newGroup, newGroup.pbegin());
-    std::set_difference(one.pcbegin(), one.pcend(), two.pcbegin(), two.pcend(), newGroupInsertIterator, TCompare());
+    Group<T> newGroup;
+    for (auto& t : one)
+        if (not two.Contains(t))
+            newGroup.Add(t);
     return newGroup;
 }
 
 //! @brief Returns group with elements that are in both groups
-template <typename T, typename TCompare>
-static Group<T, TCompare> Intersection(const Group<T, TCompare>& one, const Group<T, TCompare>& two)
+template <typename T>
+static Group<T> Intersection(const Group<T>& one, const Group<T>& two)
 {
-    Group<T, TCompare> newGroup;
-    std::insert_iterator<Group<T, TCompare>> newGroupInsertIterator(newGroup, newGroup.pbegin());
-    std::set_intersection(one.pcbegin(), one.pcend(), two.pcbegin(), two.pcend(), newGroupInsertIterator, TCompare());
+    Group<T> newGroup;
+    for (auto& t : one)
+        if (two.Contains(t))
+            newGroup.Add(t);
     return newGroup;
 }
 
 //! @brief Returns group with elements that are only in one group not in both
-template <typename T, typename TCompare>
-static Group<T, TCompare> SymmetricDifference(const Group<T, TCompare>& one, const Group<T, TCompare>& two)
+template <typename T>
+static Group<T> SymmetricDifference(const Group<T>& one, const Group<T>& two)
 {
-    Group<T, TCompare> newGroup;
-    std::insert_iterator<Group<T, TCompare>> newGroupInsertIterator(newGroup, newGroup.pbegin());
-    std::set_symmetric_difference(one.pcbegin(), one.pcend(), two.pcbegin(), two.pcend(), newGroupInsertIterator,
-                                  TCompare());
-    return newGroup;
+    return Difference(Unite(one, two), Intersection(one, two));
 }
 
 } // namespace Group
