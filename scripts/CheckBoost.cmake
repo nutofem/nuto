2c103a82edfa803368aa0b951a99e81660ff2f5a
Christoph Pohl <christophpohl@gmx.com>
2017-09-30 19:02:12 +0200

    Use IMPORTED targets syntax for boost
    Because of the `ExplicitTimeIntegration` test, all the C++ examples are
    now linked to the Boost unit test lib. This should be reverted by moving
    it to tests. Because of the runtime, solve #75 first.

--- scripts/CheckBoost.cmake
+++ scripts/CheckBoost.cmake
@@ -1,28 +1,16 @@
-message(STATUS "Checking for Boost...")
-
-set(Boost_DETAILED_FAILURE_MSG TRUE)
-
-# collect the required boost components and versions
-set(NuToBoostComponents system filesystem)
-set(NuToBoostRequiredVersion 1.49.0)
+set(Components system filesystem unit_test_framework)
+set(RequiredVersion 1.49.0)
 
 if(ENABLE_MPI)
-    set(NuToBoostComponents ${NuToBoostComponents} mpi)
+    set(Components ${Components} mpi)
 endif()
 
-set(NuToBoostComponents ${NuToBoostComponents} unit_test_framework)
+message(STATUS "Requesting BOOST components ${Components}")
+message(STATUS "At least version ${RequiredVersion} required.")
 
-message(STATUS "Requesting BOOST components ${NuToBoostComponents}")
-message(STATUS "At least version ${NuToBoostRequiredVersion} required.")
-
-find_package(Boost ${NuToBoostRequiredVersion}
-    COMPONENTS ${NuToBoostComponents} REQUIRED)
+find_package(Boost ${RequiredVersion} COMPONENTS ${Components} REQUIRED)
 
 include_directories(${Boost_INCLUDE_DIR})
 set(boostVersion
     "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}")
 message(STATUS "Boost version = ${boostVersion}")
-message(STATUS "Boost_LIBRARIES = ${Boost_LIBRARIES}")
-message(STATUS "Boost_LIBRARY_DIRS = ${Boost_LIBRARY_DIRS}")
-message(STATUS "BOOST_INCLUDE_DIR = ${BOOST_INCLUDE_DIR}")
-message(STATUS "BOOST_INCLUDE_DIRS = ${BOOST_INCLUDE_DIRS}")
