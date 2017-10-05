b1a5c0ebd72181b7149ec08f4e2da7047ae65334
Christoph Pohl <christophpohl@gmx.com>
2017-10-05 16:19:54 +0200

    Remove `ENABLE_COVERAGE`
    No need to do this locally anymore.

--- scripts/Coverage.cmake
+++
@@ -1,4 +0,0 @@
-if(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    message(FATAL_ERROR "Coverage only available when compiling with GCC.")
-endif()
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
