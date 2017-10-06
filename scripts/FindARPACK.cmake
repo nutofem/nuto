8e635600549bfed8efcc3a0bd69a178974a9c6e3
Christoph Pohl <christophpohl@gmx.com>
2017-10-06 13:20:26 +0200

    Create Arpack::Arpack target

--- scripts/FindARPACK.cmake
+++ scripts/FindARPACK.cmake
@@ -1,39 +1,13 @@
-# find the Arpack library
-#
-# variables used by this module (can be also defined as environment variables):
-#   ARPACK_ROOT - preferred installation prefix for searching for ARPACK
-#   ARPACK_DEBUG - print debug messages
-#
-# variables defined by this module
-#   ARPACK_FOUND - defines whether metis was found or not
-#   ARPACK_INCLUDE_DIR - ARPACK include directory
-#   ARPACK_LIBRARIES   - ARPACK libraries
-
-# initialize variables
+# Find the Arpack library. Creates Arpack::Arpack target.
+# ARPACK_FOUND tells you if Arpack was found.
 message(STATUS "Checking for ARPACK Library ...")
 
-# check if ARPACK_ROOT is set
-if(NOT ARPACK_ROOT AND NOT $ENV{ARPACK_ROOT} STREQUAL "")
-    set(ARPACK_ROOT $ENV{ARPACK_ROOT})
-endif()
+find_library(ARPACK_LIBRARIES NAMES arpack)
 
-# convert path to unix style path and set search path
-if(ARPACK_ROOT)
-    file(TO_CMAKE_PATH ${ARPACK_ROOT} ARPACK_ROOT)
-    set(_ARPACK_LIBRARIES_SEARCH_DIRS ${ARPACK_ROOT}/lib ${ARPACK_ROOT}
-        ${_ARPACK_LIBRARIES_SEARCH_DIRS})
-endif()
+add_library(Arpack::Arpack SHARED IMPORTED)
+set_target_properties(Arpack::Arpack PROPERTIES
+    IMPORTED_LOCATION "${ARPACK_LIBRARIES}")
 
-# search for ARPACK library
-find_library(ARPACK_LIBRARIES NAMES arpack
-    HINTS ${_ARPACK_LIBRARIES_SEARCH_DIRS})
-
-# handle the QUIETLY and REQUIRED arguments
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ARPACK DEFAULT_MSG ARPACK_LIBRARIES)
-
-if(ARPACK_DEBUG)
-    message(STATUS "ARPACK_FOUND=${ARPACK_FOUND}")
-    message(STATUS "ARPACK_LIBRARIES=${ARPACK_LIBRARIES}")
-endif()
 
