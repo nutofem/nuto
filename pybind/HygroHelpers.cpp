834a58c6f214fc75c9413cc8af5c5f4fb42afb20
Christoph Pohl <christophpohl@gmx.com>
2017-10-19 11:13:09 +0200

    Export more functions and a submodule

--- pybind/HygroHelpers.cpp
+++ pybind/HygroHelpers.cpp
@@ -4,7 +4,23 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(nuto, m) {
-    m.doc() = "NuTo Finite Element Library";
+PYBIND11_MODULE(hygro_helpers, m)
+{
+    m.doc() = "Collection of functions regarding the state of water, air and vapour.";
+
     m.def("DensityOfWater", &Hygro::DensityOfWater, "Density of water as a function of temperature");
+    m.def("SaturationPressure", &Hygro::SaturationPressure,
+          "Saturation water vapour pressure as a function of temperature");
+    m.def("DynamicViscosityOfAir", &Hygro::DynamicViscosityOfAir,
+          "Dynamic viscosity μ_g of moist air as a function of pressures and temperature.");
+    m.def("DynamicViscosityOfWater", &Hygro::DynamicViscosityOfAir,
+          "Dynamic viscosity μ_w of liquid water as a function temperature.");
+
+    auto kelvinModule = m.def_submodule("KelvinEquation", "Kelvin equation");
+    kelvinModule.def("VapourPressure", &Hygro::KelvinEquation::VapourPressure,
+                     "Vapour pressure from the Kelvin equation.");
+    kelvinModule.def("dTemperature", &Hygro::KelvinEquation::dTemperature,
+                     "First derivative of the Vapour pressure wrt to temperature");
+    kelvinModule.def("dCapillaryPressure", &Hygro::KelvinEquation::dCapillaryPressure,
+                     "First derivative of the Vapour pressure wrt to capillary pressure");
 }
