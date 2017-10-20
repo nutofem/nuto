3e10820c06724bf3d713b5777e7ee783bae1aa67
Thomas Titscher <thomas.titscher@gmail.com>
2017-10-20 08:53:44 +0200

    Add InterpolationTetrahedronLinear

---
+++ test/mechanics/interpolation/InterpolationTetrahedronLinear.cpp
@@ -0,0 +1,13 @@
+#include "BoostUnitTest.h"
+#include "InterpolationTests.h"
+#include "mechanics/interpolation/InterpolationTetrahedronLinear.h"
+
+std::vector<Eigen::VectorXd> GetTestPoints()
+{
+    return {Eigen::Vector3d(1. / 3., 1. / 3., 1. / 3.)};
+}
+
+BOOST_AUTO_TEST_CASE(InterpolationTetrahedronLinear)
+{
+    NuTo::Test::RunTests<NuTo::InterpolationTetrahedronLinear>(GetTestPoints());
+}
