a87ae136e824bea216e9bea46e4e97723a0b76b1
Thomas Titscher <thomas.titscher@gmail.com>
2017-10-24 17:29:50 +0200

    Add DofNumbering test including edge cases

--- src/mechanics/dofs/DofNumbering.cpp
+++ src/mechanics/dofs/DofNumbering.cpp
@@ -15,13 +15,35 @@ int InitialUnconstrainedNumbering(const Groups::Group<NodeSimple>& nodes)
 
 std::vector<bool> FindConstrainedDofs(const ConstraintPde::Constraints& constraints, DofType dof, int numDofs)
 {
-    std::vector<bool> isConstrained(numDofs, false);
+    std::vector<bool> isDependent(numDofs, false);
     for (int iEquation = 0; iEquation < constraints.GetNumEquations(dof); ++iEquation)
     {
         int dependentDofNumber = constraints.GetEquation(dof, iEquation).GetDependentDofNumber();
-        isConstrained[dependentDofNumber] = true;
+        if (isDependent[dependentDofNumber])
+            throw NuTo::Exception(
+                    __PRETTY_FUNCTION__,
+                    "Dof number " + std::to_string(dependentDofNumber) +
+                            " already constrained. You are only allowed to have a dependent dof in one equation.");
+        isDependent[dependentDofNumber] = true;
     }
-    return isConstrained;
+
+    // go through all terms that are not dependent and check if they contain dependent dofs
+    for (int iEquation = 0; iEquation < constraints.GetNumEquations(dof); ++iEquation)
+    {
+        for (int iTerm = 1; iTerm < constraints.GetEquation(dof, iEquation).GetTerms().size(); ++iTerm)
+        {
+            const ConstraintPde::Term& term = constraints.GetEquation(dof, iEquation).GetTerms()[iTerm];
+            int termDofNumber = term.GetConstrainedDofNumber();
+
+            if (isDependent[termDofNumber])
+                throw NuTo::Exception(__PRETTY_FUNCTION__,
+                                      "Term " + std::to_string(iTerm) + " of Equation " + std::to_string(iEquation) +
+                                              " contains dof number " + std::to_string(termDofNumber) +
+                                              " that is a dependent dof number. This is not allowed!");
+        }
+    }
+
+    return isDependent;
 }
 
 DofNumbering::DofInfo DofNumbering::Build(const Groups::Group<NodeSimple>& dofNodes, DofType dof,
@@ -31,7 +53,7 @@ DofNumbering::DofInfo DofNumbering::Build(const Groups::Group<NodeSimple>& dofNo
 
     const int numDofs = InitialUnconstrainedNumbering(dofNodes);
 
-    std::vector<bool> isConstrained = FindConstrainedDofs(constraints, dof, numDofs);
+    std::vector<bool> isDependent = FindConstrainedDofs(constraints, dof, numDofs);
 
     int countIndependentDofs = 0;
     int countDependentDofs = numDofs - numDependentDofs;
@@ -39,7 +61,7 @@ DofNumbering::DofInfo DofNumbering::Build(const Groups::Group<NodeSimple>& dofNo
         for (int iComponent = 0; iComponent < node.GetNumValues(); ++iComponent)
         {
             int dofNumber = node.GetDofNumber(iComponent);
-            if (isConstrained[dofNumber])
+            if (isDependent[dofNumber])
                 node.SetDofNumber(iComponent, countDependentDofs++);
             else
                 node.SetDofNumber(iComponent, countIndependentDofs++);
---
+++ test/mechanics/dofs/DofNumbering.cpp
@@ -0,0 +1,71 @@
+#include "BoostUnitTest.h"
+
+#include "mechanics/dofs/DofNumbering.h"
+#include <set>
+
+using namespace NuTo;
+
+auto rhs = [](double) { return 42; };
+
+const DofType d("stuff", 2);
+
+BOOST_AUTO_TEST_CASE(DofNumberingTest)
+{
+    /* Add three 2d nodes, one of them constrained in x and y.
+     * Check if unconstrained nodes have dof number [0..3] and
+     * constrained nodes have dof number [4..5].
+     */
+    NodeSimple nodeUnconstrained0(Eigen::Vector2d::Zero());
+    NodeSimple nodeUnconstrained1(Eigen::Vector2d::Zero());
+    NodeSimple nodeConstrained(Eigen::Vector2d::Zero());
+
+    ConstraintPde::Constraints constraints;
+
+    Groups::Group<NodeSimple> group({nodeConstrained, nodeUnconstrained0, nodeUnconstrained1});
+
+    constraints.Add(d, {nodeConstrained, 0, rhs});
+    constraints.Add(d, {nodeConstrained, 1, rhs});
+
+    auto dofInfo = DofNumbering::Build(group, d, constraints);
+    BOOST_CHECK_EQUAL(dofInfo.numDependentDofs[d], 2);
+    BOOST_CHECK_EQUAL(dofInfo.numIndependentDofs[d], 4);
+
+    std::set<int> dofNumbers{nodeUnconstrained0.GetDofNumber(0), nodeUnconstrained0.GetDofNumber(1),
+                             nodeUnconstrained1.GetDofNumber(0), nodeUnconstrained1.GetDofNumber(1)};
+    BOOST_CHECK_EQUAL(dofNumbers.size(), 4);
+    BOOST_CHECK_EQUAL(*dofNumbers.rbegin(), 3);
+
+    dofNumbers.insert(nodeConstrained.GetDofNumber(0));
+    dofNumbers.insert(nodeConstrained.GetDofNumber(1));
+    BOOST_CHECK_EQUAL(dofNumbers.size(), 6);
+    BOOST_CHECK_EQUAL(*dofNumbers.rbegin(), 5);
+}
+
+BOOST_AUTO_TEST_CASE(DoubleConstrained)
+{
+    ConstraintPde::Constraints constraints;
+    NodeSimple node(Eigen::Vector2d::Zero());
+
+    Groups::Group<NodeSimple> group(node);
+
+    constraints.Add(d, {node, 0, rhs});
+    constraints.Add(d, {node, 0, rhs});
+
+    BOOST_CHECK_THROW(DofNumbering::Build(group, d, constraints), Exception);
+}
+
+BOOST_AUTO_TEST_CASE(TwoDependentDofsInOneEquation)
+{
+    ConstraintPde::Constraints constraints;
+    NodeSimple node(Eigen::Vector2d::Zero());
+
+    Groups::Group<NodeSimple> group(node);
+
+    constraints.Add(d, {node, 0, rhs}); // node.dof0 * 1.0 = rhs
+
+    ConstraintPde::Equation equation(node, 1, rhs);
+    equation.AddTerm({node, 0, 0.4}); // node.dof1 * 1.0 + node.dof0 * 0.4 = rhs
+    constraints.Add(d, equation);
+
+    BOOST_CHECK_THROW(DofNumbering::Build(group, d, constraints), Exception);
+}
