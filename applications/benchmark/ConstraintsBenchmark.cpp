2eed32bc5e4d1d09388ba75474adf2f186083500
Thomas Titscher <thomas.titscher@gmail.com>
2017-10-25 11:18:28 +0200

    Add benchmark, proves that checks are O(N^2)
    This is way too slow for >10000 constraints.

---
+++ applications/benchmark/ConstraintsBenchmark.cpp
@@ -0,0 +1,40 @@
+#include "Benchmark.h"
+#include "mechanics/constraintsPde/Constraints.h"
+
+auto zero = [](double) { return 0.; };
+const NuTo::DofType d("...", 1);
+
+
+void Run(BenchmarkInternal::Runner& runner, int size)
+{
+    std::vector<NuTo::NodeSimple> nodes(size, NuTo::NodeSimple(0));
+    for (int i = 0; i < nodes.size(); ++i)
+        nodes[i].SetDofNumber(0, i);
+
+    while (runner.KeepRunningTime(1))
+    {
+        NuTo::ConstraintPde::Constraints c;
+        for (int i = 0; i < nodes.size(); ++i)
+            c.Add(d, {nodes[i], 0, zero});
+    }
+}
+
+BENCHMARK(Constraints, c100, runner)
+{
+    Run(runner, 100);
+};
+
+BENCHMARK(Constraints, c1000, runner)
+{
+    Run(runner, 1000);
+};
+
+BENCHMARK(Constraints, c10000, runner)
+{
+    Run(runner, 10000);
+};
+
+BENCHMARK(Constraints, c100000, runner)
+{
+    Run(runner, 100000);
+};
