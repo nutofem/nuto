91c0d950dab00c9e62b8dd0cfaf0df06c846211d
Volker Hirthammer <volker.hirthammer@gmail.com>
2017-10-26 12:46:06 +0200

    Started rewriting ConstraintCompanion for PDE
    - renamed test ConstraintCompanian in PDE folder to ConstraintCompanionPde
    - added test to CMakeLists.txt
    - Got Component and Rhs functions to compile

--- src/mechanics/constraintsPde/ConstraintCompanion.h
+++ src/mechanics/constraintsPde/ConstraintCompanion.h
@@ -1,14 +1,17 @@
 #pragma once
 
-#include "mechanics/constraints/Constraints.h"
+#include "base/Group.h"
+#include "mechanics/constraintsPde/Constraints.h"
+#include "mechanics/constraintsPde/Equation.h"
 #include "mechanics/DirectionEnum.h"
+#include "mechanics/nodes/NodeSimple.h"
 
 namespace NuTo
 {
 template <typename T>
 class Group;
 
-namespace Constraint
+namespace ConstraintPde
 {
 
 //! @brief linear function
@@ -35,72 +38,75 @@ inline RhsFunction RhsConstant(double constantValue)
 //! @param directions ... vector of directions (X, Y, Z)
 //! @param value ... constant value
 //! @return ... vector of constraint equations
-std::vector<Equation> Component(const NodeBase& node, std::vector<eDirection> directions, double value = 0.0);
+std::vector<Equation> Component(const NodeSimple& node, std::vector<eDirection> directions, double value = 0.0);
 
 //! @brief constraints components of vector valued nodes in X and/or Y and/or Z
 //! @param nodes ... group of nodes
 //! @param directions ... vector of directions (X, Y, Z)
 //! @param value ... constant value
 //! @return ... vector of constraint equations
-std::vector<Equation> Component(const Group<NodeBase>& nodes, std::vector<eDirection> directions, double value = 0.0);
+std::vector<Equation> Component(const Groups::Group<NodeSimple>& nodes, std::vector<eDirection> directions,
+                                double value = 0.0);
 
 //! @brief constraints components of vector valued nodes in X and/or Y and/or Z
 //! @param node ... node reference
 //! @param directions ... vector of directions (X, Y, Z)
 //! @param rhs ... time dependent constraint function (double time) --> double
 //! @return ... vector of constraint equations
-std::vector<Equation> Component(const NodeBase& node, std::vector<eDirection> directions, RhsFunction rhs);
+std::vector<Equation> Component(const NodeSimple& node, std::vector<eDirection> directions, RhsFunction rhs);
 
 //! @brief constraints components of vector valued nodes in X and/or Y and/or Z
 //! @param nodes ... group of nodes
 //! @param directions ... vector of directions (X, Y, Z)
 //! @param rhs ... time dependent constraint function (double time) --> double
 //! @return ... vector of constraint equations
-std::vector<Equation> Component(const Group<NodeBase>& nodes, std::vector<eDirection> directions, RhsFunction rhs);
+std::vector<Equation> Component(const Groups::Group<NodeSimple>& nodes, std::vector<eDirection> directions,
+                                RhsFunction rhs);
 
-//! @param node ... node reference
-//! @param direction ... directions (X, Y, Z)
-//! @param rhs ... time dependent constraint function (double time) --> double
-//! @return ... constraint equation
-Equation Direction(const NodeBase& node, Eigen::VectorXd direction, RhsFunction rhs);
+////! @param node ... node reference
+////! @param direction ... directions (X, Y, Z)
+////! @param rhs ... time dependent constraint function (double time) --> double
+////! @return ... constraint equation
+// Equation Direction(const NodeSimple& node, Eigen::VectorXd direction, RhsFunction rhs);
 
-//! @param node ... node reference
-//! @param direction ... directions (X, Y, Z)
-//! @param value ... constant value
-//! @return ... constraint equation
-Equation Direction(const NodeBase& node, Eigen::VectorXd direction, double value = 0.0);
+////! @param node ... node reference
+////! @param direction ... directions (X, Y, Z)
+////! @param value ... constant value
+////! @return ... constraint equation
+// Equation Direction(const NodeSimple& node, Eigen::VectorXd direction, double value = 0.0);
 
-//! @param nodes ... group of nodes
-//! @param direction ... directions (X, Y, Z)
-//! @param rhs ... time dependent constraint function (double time) --> double
-//! @return ... vector of constraint equations
-std::vector<Equation> Direction(const Group<NodeBase>& nodes, Eigen::VectorXd direction, RhsFunction rhs);
+////! @param nodes ... group of nodes
+////! @param direction ... directions (X, Y, Z)
+////! @param rhs ... time dependent constraint function (double time) --> double
+////! @return ... vector of constraint equations
+// std::vector<Equation> Direction(constGroups::Group<NodeSimple>& nodes, Eigen::VectorXd direction, RhsFunction rhs);
 
-//! @param nodes ... group of nodes
-//! @param direction ... directions (X, Y, Z)
-//! @param value ... constant value
-//! @return vector of constraint equations
-std::vector<Equation> Direction(const Group<NodeBase>& nodes, Eigen::VectorXd direction, double value = 0.0);
+////! @param nodes ... group of nodes
+////! @param direction ... directions (X, Y, Z)
+////! @param value ... constant value
+////! @return vector of constraint equations
+// std::vector<Equation> Direction(constGroups::Group<NodeSimple>& nodes, Eigen::VectorXd direction, double value =
+// 0.0);
 
-//! @param node ... node reference
-//! @param value ... constant value
-//! @return ... constraint equation
-Equation Value(const NodeBase& node, double value = 0.0);
+////! @param node ... node reference
+////! @param value ... constant value
+////! @return ... constraint equation
+// Equation Value(const NodeSimple& node, double value = 0.0);
 
-//! @param node ... node reference
-//! @param rhs ... time dependent constraint function (double time) --> double
-//! @return ... constraint equation
-Equation Value(const NodeBase& node, RhsFunction rhs);
+////! @param node ... node reference
+////! @param rhs ... time dependent constraint function (double time) --> double
+////! @return ... constraint equation
+// Equation Value(const NodeSimple& node, RhsFunction rhs);
 
-//! @param nodes ... group of nodes
-//! @param value ... constant value
-//! @return ... vector of constraint equations
-std::vector<Equation> Value(const Group<NodeBase>& nodes, double value = 0.0);
+////! @param nodes ... group of nodes
+////! @param value ... constant value
+////! @return ... vector of constraint equations
+// std::vector<Equation> Value(constGroups::Group<NodeSimple>& nodes, double value = 0.0);
 
-//! @param nodes ... group of nodes
-//! @param rhs ... time dependent constraint function (double time) --> double
-//! @return ... vector of constraint equations
-std::vector<Equation> Value(const Group<NodeBase>& nodes, RhsFunction rhs);
+////! @param nodes ... group of nodes
+////! @param rhs ... time dependent constraint function (double time) --> double
+////! @return ... vector of constraint equations
+// std::vector<Equation> Value(constGroups::Group<NodeSimple>& nodes, RhsFunction rhs);
 
 } /* Constraint */
 } /* NuTo */
---
+++ test/mechanics/constraintsPde/ConstraintCompanionPde.cpp
@@ -0,0 +1,182 @@
+#include "BoostUnitTest.h"
+//#include <fakeit.hpp>
+//#include <cmath>
+//#include "mechanics/groups/Group.h"
+#include "mechanics/constraintsPde/ConstraintCompanion.h"
+
+using namespace NuTo;
+using namespace NuTo::ConstraintPde;
+
+
+//// Helper functions for comparisons
+// namespace NuTo
+//{
+// namespace ConstraintPde
+//{
+// bool operator==(const Term& a, const Term& b)
+//{
+//    if (&(a.GetNode()) != &(b.GetNode()))
+//        return false;
+//    if (a.GetCoefficient() != b.GetCoefficient())
+//        return false;
+//    if (a.GetComponent() != b.GetComponent())
+//        return false;
+//    return true;
+//}
+
+// bool operator==(const Equation& a, const Equation& b)
+//{
+//    if (a.GetRhs(0.0) != b.GetRhs(0.0))
+//        return false;
+//    if (a.GetRhs(42.0) != b.GetRhs(42.0))
+//        return false;
+//    if (a.GetTerms() != b.GetTerms())
+//        return false;
+//    return true;
+//}
+
+//} // Constraint
+//} // NuTo
+
+
+//// quite the stupid tests, but oh well...
+// BOOST_AUTO_TEST_CASE(PredefinedFunctions)
+//{
+//    auto ramp = Constraint::RhsRamp(42.0, 6174.0);
+//    BOOST_CHECK_EQUAL(ramp(0.0), 0.0);
+//    BOOST_CHECK_EQUAL(ramp(42.0), 6174.0);
+
+//    auto constant = Constraint::RhsConstant(42.0);
+//    BOOST_CHECK_EQUAL(constant(0.0), 42.0);
+//    BOOST_CHECK_EQUAL(constant(42.0), 42.0);
+//}
+
+
+// Mock<NuTo::NodeBase> nodeMock;
+
+//// mock node, nodegroup, direction vector and lamdba function
+// class Helpers
+//{
+// public:
+//    Helpers()
+//        : node(nodeMock.get())
+//    {
+//        When(Method(nodeMock, GetNumDofs)).AlwaysReturn(3);
+//        nodes.AddMember(0, &nodeMock.get());
+//        nodes.AddMember(1, &nodeMock.get());
+//        someDirection << 1, 1, 1;
+//    }
+//    std::function<double(double)> sinFunction = [](double time) { return std::sin(time); };
+//    Group<NodeBase> nodes;
+//    NodeBase& node;
+//    Eigen::Vector3d someDirection;
+//    double reciprocalNorm = 1.0 / std::sqrt(3.0);
+//};
+
+
+// BOOST_FIXTURE_TEST_CASE(Component, Helpers)
+//{
+//    // with constant RHS
+//    auto eqs = Constraint::Component(node, {eDirection::X});
+//    auto expectedEquation = Constraint::Equation({Constraint::Term(node, 0, 1.0)});
+//    BOOST_CHECK(eqs[0] == expectedEquation);
+
+//    // with lambda RHS
+//    eqs = Constraint::Component(node, {eDirection::X}, sinFunction);
+//    expectedEquation = Constraint::Equation({Constraint::Term(node, 0, 1.0)}, sinFunction);
+//    BOOST_CHECK(eqs[0] == expectedEquation);
+
+//    // with Group
+//    std::vector<Constraint::Equation> expectedEquations;
+//    eqs = Constraint::Component(nodes, {eDirection::X});
+//    for (auto& node : nodes)
+//    {
+//        expectedEquations.push_back(Constraint::Equation({Constraint::Term(*node.second, 0, 1.0)}));
+//    }
+//    BOOST_CHECK(eqs == expectedEquations);
+
+//    // with Group and lambda
+//    expectedEquations.clear();
+//    eqs = Constraint::Component(nodes, {eDirection::X}, sinFunction);
+//    for (auto& node : nodes)
+//    {
+//        expectedEquations.push_back(Constraint::Equation({Constraint::Term(*node.second, 0, 1.0)}, sinFunction));
+//    }
+//    BOOST_CHECK(eqs == expectedEquations);
+//}
+
+
+// BOOST_FIXTURE_TEST_CASE(Direction, Helpers)
+//{
+//    // with constant RHS
+//    auto eq = Constraint::Direction(node, someDirection);
+//    auto expectedEquation =
+//            Constraint::Equation({Constraint::Term(node, 0, reciprocalNorm), Constraint::Term(node, 1,
+//            reciprocalNorm),
+//                                  Constraint::Term(node, 2, reciprocalNorm)});
+//    BOOST_CHECK(eq == expectedEquation);
+
+//    // with lambda RHS
+//    eq = Constraint::Direction(node, someDirection, sinFunction);
+//    expectedEquation =
+//            Constraint::Equation({Constraint::Term(node, 0, reciprocalNorm), Constraint::Term(node, 1,
+//            reciprocalNorm),
+//                                  Constraint::Term(node, 2, reciprocalNorm)},
+//                                 sinFunction);
+//    BOOST_CHECK(eq == expectedEquation);
+
+//    // with Group
+//    std::vector<Constraint::Equation> expectedEquations;
+//    auto eqs = Constraint::Direction(nodes, someDirection);
+//    for (auto& node : nodes)
+//    {
+//        expectedEquations.push_back(Constraint::Equation({Constraint::Term(*node.second, 0, reciprocalNorm),
+//                                                          Constraint::Term(*node.second, 1, reciprocalNorm),
+//                                                          Constraint::Term(*node.second, 2, reciprocalNorm)}));
+//    }
+//    BOOST_CHECK(eqs == expectedEquations);
+
+//    // with Group and lambda
+//    expectedEquations.clear();
+//    eqs = Constraint::Direction(nodes, someDirection, sinFunction);
+//    for (auto& node : nodes)
+//    {
+//        expectedEquations.push_back(Constraint::Equation({Constraint::Term(*node.second, 0, reciprocalNorm),
+//                                                          Constraint::Term(*node.second, 1, reciprocalNorm),
+//                                                          Constraint::Term(*node.second, 2, reciprocalNorm)},
+//                                                         sinFunction));
+//    }
+//    BOOST_CHECK(eqs == expectedEquations);
+//}
+
+
+// BOOST_FIXTURE_TEST_CASE(Value, Helpers)
+//{
+//    // with constant RHS
+//    auto eq = Constraint::Value(node);
+//    auto expectedEquation = Constraint::Equation({Constraint::Term(node, 0, 1.0)});
+//    BOOST_CHECK(eq == expectedEquation);
+
+//    // with lamdba RHS
+//    eq = Constraint::Value(node, sinFunction);
+//    expectedEquation = Constraint::Equation({Constraint::Term(node, 0, 1.0)}, sinFunction);
+//    BOOST_CHECK(eq == expectedEquation);
+
+//    // with Group
+//    auto eqs = Constraint::Value(nodes);
+//    std::vector<Constraint::Equation> expectedEquations;
+//    for (auto& node : nodes)
+//    {
+//        expectedEquations.push_back(Constraint::Equation({Constraint::Term(*node.second, 0, 1.0)}));
+//    }
+//    BOOST_CHECK(eqs == expectedEquations);
+
+//    // with Group
+//    eqs = Constraint::Value(nodes, sinFunction);
+//    expectedEquations.clear();
+//    for (auto& node : nodes)
+//    {
+//        expectedEquations.push_back(Constraint::Equation({Constraint::Term(*node.second, 0, 1.0)}, sinFunction));
+//    }
+//    BOOST_CHECK(eqs == expectedEquations);
+//}
