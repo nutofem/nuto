1e2e829f71acc6e3f2d81ba12b12a2429d1bc0ff
Thomas Titscher <thomas.titscher@gmail.com>
2017-11-06 16:59:28 +0100

    Propose an alternative test for Jacobian2Din3D
    Uses the Jacobian::Det() as a area ratio to calculate the area of a
    triangle in 3D.

--- test/mechanics/cell/Jacobian.cpp
+++ test/mechanics/cell/Jacobian.cpp
@@ -301,6 +301,42 @@ BOOST_AUTO_TEST_CASE(Jacobian2Din3D_TetrahedronQuadratic)
     }
 }
 
+//! @brief calculate the area of a triangle (points a,b,c) element based on ||J|| and check vs. `correctArea`
+void CheckJacobian2Din3D(Eigen::Vector3d a, Eigen::Vector3d b, Eigen::Vector3d c, double correctArea)
+{
+    Eigen::MatrixXd B = NuTo::ShapeFunctions2D::DerivativeShapeFunctionsTriangleOrder1(Eigen::Vector2d::Zero());
+    Eigen::VectorXd coordinates = Eigen::VectorXd(12);
+    coordinates << a.x(), a.y(), a.z(), b.x(), b.y(), b.z(), c.x(), c.y(), c.z();
+
+    NuTo::Jacobian jacobian(coordinates, B, 3);
+
+    double ratioAreaRealToAreaNatural = jacobian.Det();
+    double areaNatural = 0.5; // triangle (0,0) ; (1,0) ; (0,1)
+    double calculatedArea = ratioAreaRealToAreaNatural * areaNatural;
+
+    BOOST_CHECK_CLOSE(correctArea, calculatedArea, 1.e-10);
+}
+
+BOOST_AUTO_TEST_CASE(Jacobian2Din3D)
+{
+    // some manual test cases
+    CheckJacobian2Din3D(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(1, 0, 0), Eigen::Vector3d(0, 1, 0), 0.5);
+    CheckJacobian2Din3D(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(5, 0, 0), Eigen::Vector3d(0, 0, 7), 5. * 7 / 2);
+    CheckJacobian2Din3D(Eigen::Vector3d(0, 4, 0), Eigen::Vector3d(0, 0, 3), Eigen::Vector3d(0, 4, 3), 3. * 4 / 2);
+
+    // some random test cases
+    for (int i = 0; i < 100; ++i)
+    {
+        // arbitrary 3d points A, B, C define a triangle (A,B,C)
+        Eigen::Vector3d a = Eigen::Vector3d::Random();
+        Eigen::Vector3d b = Eigen::Vector3d::Random();
+        Eigen::Vector3d c = Eigen::Vector3d::Random();
+
+        double analyticalArea = 0.5 * (b - a).cross(c - a).norm(); // wiki...
+        CheckJacobian2Din3D(a, b, c, analyticalArea);
+    }
+}
+
 BOOST_AUTO_TEST_CASE(JacobianTransform)
 {
     // TODO! How?
