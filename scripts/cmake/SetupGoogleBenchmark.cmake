7cf9e74082c9182e152c5730451a2e751ba706ae
Volker Hirthammer <volker.hirthammer@gmail.com>
2017-11-16 14:50:37 +0100

    Change default options of benchmark lib

--- scripts/cmake/SetupGoogleBenchmark.cmake
+++ scripts/cmake/SetupGoogleBenchmark.cmake
@@ -3,6 +3,10 @@ function(setup_google_benchmark)
     set(ORIGINAL_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
     set(CMAKE_CXX_FLAGS "-std=c++14 -fPIC -pthread")
 
+    # Change library default options
+    option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)
+    option(BENCHMARK_ENABLE_INSTALL "Enable installation of benchmark. (Projects embedding benchmark may want to turn this OFF.)" OFF)
+
     add_subdirectory(external/benchmark EXCLUDE_FROM_ALL)
 
     # Restore nuto cxx flags
