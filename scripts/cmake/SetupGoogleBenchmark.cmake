e8db5fed926565d405c2e5a776acc75b3ce14e95
Volker Hirthammer <volker.hirthammer@gmail.com>
2017-11-16 10:20:06 +0100

    Set lib specific cxx flags for google benchmark

--- scripts/cmake/SetupGoogleBenchmark.cmake
+++ scripts/cmake/SetupGoogleBenchmark.cmake
@@ -1,3 +1,10 @@
 function(setup_google_benchmark)
-        add_subdirectory(external/benchmark EXCLUDE_FROM_ALL)
+    # Store nuto cxx flags and set library specific flags
+    set(ORIGINAL_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+    set(CMAKE_CXX_FLAGS "-std=c++14 -fPIC -pthread")
+
+    add_subdirectory(external/benchmark EXCLUDE_FROM_ALL)
+
+    # Restore nuto cxx flags
+    set(CMAKE_CXX_FLAGS "${ORIGINAL_CXX_FLAGS}")
 endfunction()
