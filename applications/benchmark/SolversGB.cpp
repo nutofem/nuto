289b5b994c643872d0682586950b81e0759a913d
Volker Hirthammer <volker.hirthammer@gmail.com>
2017-11-27 15:25:13 +0100

    Optimize SolverGB benchmark

--- applications/benchmark/SolversGB.cpp
+++ applications/benchmark/SolversGB.cpp
@@ -7,6 +7,9 @@
 #include "mechanics/dofSubMatrixSolvers/SolverPardiso.h"
 #include "mechanics/dofSubMatrixSolvers/SolverEigen.h"
 
+
+// Setup Test Structure %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 class TestProblem
 {
 public:
@@ -39,67 +42,67 @@ private:
     NuTo::BlockFullVector<double> rhs;
 };
 
-const std::vector<int> numElements = {10, 10, 10};
+// Benchmark Fixture %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+class fixture : public benchmark::Fixture
+{
+    const std::vector<int> numElements = {10, 10, 10};
+    NuTo::Benchmark::LinearElasticBenchmarkStructure s = {numElements};
+
+public:
+    TestProblem t = {s};
+
+    template <typename TSolver = void, typename... Params>
+    void SolverBenchmark(benchmark::State& state, Params&&... params)
+    {
+        TSolver solver = {std::forward<Params>(params)...};
+        for (auto _ : state)
+            t.Solve(solver);
+    }
+};
+
 
 // Conversion Benchmarks %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-static void Convert_NuToToEigen(benchmark::State& state)
+
+BENCHMARK_F(fixture, Convert_NuToToEigen)(benchmark::State& state)
 {
-    NuTo::Benchmark::LinearElasticBenchmarkStructure s(numElements);
-    TestProblem t(s);
     for (auto _ : state)
         t.NuToToEigen();
 }
-BENCHMARK(Convert_NuToToEigen);
 
 
-static void Convert_NuToToCSR(benchmark::State& state)
+BENCHMARK_F(fixture, Convert_NuToToCSR)(benchmark::State& state)
 {
-    NuTo::Benchmark::LinearElasticBenchmarkStructure s(numElements);
-    TestProblem t(s);
     for (auto _ : state)
         t.NuToToCSR();
 }
-BENCHMARK(Convert_NuToToCSR);
 
 
 // Solver Benchmarks %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-template <typename TSolver, typename... Params>
-static void SolverBenchmark(benchmark::State& state, Params&&... params)
-{
-    NuTo::Benchmark::LinearElasticBenchmarkStructure s(numElements);
-    TestProblem t(s);
-    TSolver solver(std::forward<Params>(params)...);
-    for (auto _ : state)
-        t.Solve(solver);
-}
-
-static void Solver_MUMPS(benchmark::State& state)
+BENCHMARK_F(fixture, Solver_MUMPS)(benchmark::State& state)
 {
     SolverBenchmark<NuTo::SolverMUMPS>(state, false);
 }
-BENCHMARK(Solver_MUMPS);
+
 
 #ifdef HAVE_PARDISO
-static void Solver_Pardiso(Solver_Pardiso)
+BENCHMARK_F(fixture, Solver_Pardiso)(Solver_Pardiso)
 {
     SolverBenchmark<NuTo::SolverPardiso>(state, 1, false);
 }
-BENCHMARK(Solver_Pardiso);
 #endif /* HAVE_PARDISO */
 
 
-static void Solver_EigenLU(benchmark::State& state)
+BENCHMARK_F(fixture, Solver_EigenLU)(benchmark::State& state)
 {
     SolverBenchmark<NuTo::SolverEigen<Eigen::SparseLU<Eigen::SparseMatrix<double>, Eigen::COLAMDOrdering<int>>>>(state);
 }
-BENCHMARK(Solver_EigenLU);
 
-static void Solver_EigenLDLT(benchmark::State& state)
+BENCHMARK_F(fixture, Solver_EigenLDLT)(benchmark::State& state)
 {
     SolverBenchmark<NuTo::SolverEigen<Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>>>(state);
 }
-BENCHMARK(Solver_EigenLDLT);
 
 
 BENCHMARK_MAIN()
