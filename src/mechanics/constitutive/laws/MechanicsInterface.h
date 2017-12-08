d41608929f098788c08907aae0545de27c719f0b
Volker Hirthammer <volker.hirthammer@gmail.com>
2017-12-08 12:21:06 +0100

    Fix naming inconsistency

--- src/mechanics/constitutive/laws/MechanicsInterface.h
+++ src/mechanics/constitutive/laws/MechanicsInterface.h
@@ -11,9 +11,9 @@ template <int TDim>
 struct MechanicsInterface
 {
     using MechanicsTangent = Eigen::Matrix<double, Voigt::Dim(TDim), Voigt::Dim(TDim)>;
-    virtual EngineeringStressPDE<TDim> Stress(EngineeringStrainPDE<TDim> strain, double deltaT, int cellNum,
+    virtual EngineeringStressPDE<TDim> Stress(EngineeringStrainPDE<TDim> strain, double deltaT, int cellId,
                                               int ipNum) const = 0;
-    virtual MechanicsTangent Tangent(EngineeringStrainPDE<TDim>, double deltaT, int cellNum, int ipNum) const = 0;
+    virtual MechanicsTangent Tangent(EngineeringStrainPDE<TDim>, double deltaT, int cellId, int ipId) const = 0;
 };
 } /* Laws */
 } /* NuTo */
