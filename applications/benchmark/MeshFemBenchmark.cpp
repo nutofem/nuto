34227384b4433b376775eca9fae666a2eda3f69b
Thomas Titscher <thomas.titscher@bam.de>
2017-12-16 07:54:54 +0100

    Fix overflow bug in MeshFemBenchmark
    Previously, the transform benchmark took one mesh and transformed it
    (e.g. with a factor of 12) _very often_. So 12**numIterations. This
    resulted (IMO) in floating point errors. Fixed that by creating a new
    mesh in the benchmark loop but only measuring the time for the actual
    transformation.

--- applications/benchmark/MeshFemBenchmark.cpp
+++ applications/benchmark/MeshFemBenchmark.cpp
@@ -1,4 +1,5 @@
 #include <benchmark/benchmark.h>
+#include <chrono>
 
 #include "mechanics/mesh/MeshFemDofConvert.h"
 #include "mechanics/mesh/UnitMeshFem.h"
@@ -6,9 +7,13 @@
 
 /*
  * Calculates the big O complexity of various, potentially expensive MeshFem methods.
+ *
+ * It may be a good example on how to use manual timing. Not all work done inside the
+ * benchmarks loops is supposed to contribute to the big O complexity. These parts
+ * are excluded from the measurement.
  */
 
-//! @brief Measures to create a 2D triangle mesh
+//! @brief Measures time to create a 2D triangle mesh
 static void Create(benchmark::State& state)
 {
     const int n = state.range(0);
@@ -17,7 +22,7 @@ static void Create(benchmark::State& state)
     state.SetComplexityN(n * n);
 }
 
-//! @brief Measures to create a 2D triangle mesh and add another 'layer' of dof elements
+//! @brief Measures time add another 'layer' of dof elements
 static void Convert(benchmark::State& state)
 {
     const int n = state.range(0);
@@ -25,12 +30,17 @@ static void Convert(benchmark::State& state)
     for (auto _ : state)
     {
         NuTo::MeshFem mesh = NuTo::UnitMeshFem::CreateTriangles(n, n);
+        auto start = std::chrono::high_resolution_clock::now();
         NuTo::AddDofInterpolation(&mesh, NuTo::DofType("dof", 2), interpolation);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+        state.SetIterationTime(elapsed_seconds.count());
     }
     state.SetComplexityN(n * n);
 }
 
-//! @brief Measures to transform the coordinates of a 2D triangle mesh
+//! @brief Measures time to transform the coordinates of a 2D triangle mesh
 static void Transform(benchmark::State& state)
 {
     const int n = state.range(0);
@@ -39,10 +49,15 @@ static void Transform(benchmark::State& state)
         return Eigen::Vector2d(oldCoords[0] * 12 + 1, oldCoords[1] * 4 - 6174);
     };
 
-    NuTo::MeshFem mesh = NuTo::UnitMeshFem::CreateTriangles(n, n);
     for (auto _ : state)
     {
+        NuTo::MeshFem mesh = NuTo::UnitMeshFem::CreateTriangles(n, n);
+        auto start = std::chrono::high_resolution_clock::now();
         NuTo::MeshFem newMesh = NuTo::UnitMeshFem::Transform(std::move(mesh), f);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+        state.SetIterationTime(elapsed_seconds.count());
     }
     state.SetComplexityN(n * n);
 }
