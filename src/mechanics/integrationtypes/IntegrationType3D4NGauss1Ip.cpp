e39f63ed48efdd54a4e456095f5204aad48c5c16
Christoph Pohl <christophpohl@gmx.com>
2017-12-22 11:58:14 +0100

    Revert "Activate Release mode checks"
    This reverts commit f779d276bda214a41bb34230825c2cdc0904d828.

--- src/mechanics/integrationtypes/IntegrationType3D4NGauss1Ip.cpp
+++ src/mechanics/integrationtypes/IntegrationType3D4NGauss1Ip.cpp
@@ -1,17 +1,31 @@
+#include <cassert>
 #include "mechanics/integrationtypes/IntegrationType3D4NGauss1Ip.h"
 
-Eigen::VectorXd NuTo::IntegrationType3D4NGauss1Ip::GetLocalIntegrationPointCoordinates(int) const
+//! @brief constructor
+NuTo::IntegrationType3D4NGauss1Ip::IntegrationType3D4NGauss1Ip()
 {
+}
+
+//! @brief returns the local coordinates of an integration point
+//! @param rIpNum integration point (counting from zero)
+//! @param rCoordinates (result)
+Eigen::VectorXd NuTo::IntegrationType3D4NGauss1Ip::GetLocalIntegrationPointCoordinates(int rIpNum) const
+{
+    assert(rIpNum == 0);
     return Eigen::Vector3d({0.25, 0.25, 0.25});
 }
 
 
+//! @brief returns the total number of integration points for this integration type
+//! @return number of integration points
 int NuTo::IntegrationType3D4NGauss1Ip::GetNumIntegrationPoints() const
 {
     return 1;
 }
 
-
+//! @brief returns the weight of an integration point
+//! @param rIpNum integration point (counting from zero)
+//! @return weight of integration points
 double NuTo::IntegrationType3D4NGauss1Ip::GetIntegrationPointWeight(int) const
 {
     return 1 / 6.;
--- src/mechanics/integrationtypes/IntegrationType3D6NGauss1Ip.cpp
+++ src/mechanics/integrationtypes/IntegrationType3D6NGauss1Ip.cpp
@@ -1,18 +1,24 @@
+#include <cassert>
 #include "mechanics/integrationtypes/IntegrationType3D6NGauss1Ip.h"
 
 
-Eigen::VectorXd NuTo::IntegrationType3D6NGauss1Ip::GetLocalIntegrationPointCoordinates(int) const
+Eigen::VectorXd NuTo::IntegrationType3D6NGauss1Ip::GetLocalIntegrationPointCoordinates(int rIpNum) const
 {
+    assert(rIpNum == 0);
     return Eigen::Vector3d({1. / 3., 1. / 3., 0});
 }
 
 
+//! @brief returns the total number of integration points for this integration type
+//! @return number of integration points
 int NuTo::IntegrationType3D6NGauss1Ip::GetNumIntegrationPoints() const
 {
     return 1;
 }
 
-
+//! @brief returns the weight of an integration point
+//! @param rIpNum integration point (counting from zero)
+//! @return weight of integration points
 double NuTo::IntegrationType3D6NGauss1Ip::GetIntegrationPointWeight(int) const
 {
     return 1;
