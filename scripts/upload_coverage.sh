83db65dabd72be8863bdb03bd8273406b16c05e9
Thomas Titscher <thomas.titscher@gmail.com>
2018-01-03 17:57:50 +0100

    Run travis using docker image

--- scripts/upload_coverage.sh
+++ scripts/upload_coverage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ev
-if [[ "$COVERAGE" == "--coverage" ]]; then
+#if [[ "$COVERAGE" == "--coverage" ]]; then
     # get coverage information
-    lcov --capture --directory build --output-file coverage.info 2> /dev/null
+    lcov --capture --directory . --output-file coverage.info # 2> /dev/null
     # filter out system stuff that we don't control
     lcov -r coverage.info '/usr/include/*' -o coverage.info
     # filter out external libraries
     EXTERNALFILES="$(pwd)/external/*"
     lcov -r coverage.info "$EXTERNALFILES" -o coverage.info
     # upload to codecov
-    bash <(curl -s https://codecov.io/bash) -f coverage.info > /dev/null 2> /dev/null
-fi
+    bash <(curl -s https://codecov.io/bash) -f coverage.info # > /dev/null 2> /dev/null
+#fi
