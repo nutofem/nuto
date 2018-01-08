c2119816ea5408748537bbd2e0879c1336c60737
Christoph Pohl <christophpohl@gmx.com>
2018-01-08 10:34:36 +0100

    Add documentation for visualize classes
    [ci skip]

--- src/visualize/QuadAverageHandler.h
+++ src/visualize/QuadAverageHandler.h
@@ -8,14 +8,28 @@ namespace NuTo
 namespace Visualize
 {
 
+//! Cell handler that averages the cell values over each cell.
 class QuadAverageHandler
 {
 public:
+    //! Generate a visualize geometry for each cell and write it to the grid.
+    //! @param cell Current cell to be visualized.
+    //! @param grid Pointer to the grid where the geometry should be written to.
     std::vector<int> WriteGeometry(const CellInterface& cell, UnstructuredGrid* grid);
 
+    //! Write DOF values into the grid.
+    //! @param cell Current cell to be visualized.
+    //! @param dof DofType to be visualized.
+    //! @param pointIds IDs of the visualization points belonging to the cell.
+    //! @param grid Pointer to the grid where the geometry should be written to.
     void WriteDofValues(const CellInterface& cell, const DofType dof, std::vector<int> pointIds,
                         UnstructuredGrid* grid);
 
+    //! Write cell data into the grid.
+    //! @param cellId ID of the cell to be visualized.
+    //! @param values List of values to be visualized (e.g. integration point values).
+    //! @param name Name to be used in the resulting output file for the data array.
+    //! @param grid Pointer to the grid where the geometry should be written to.
     void CellData(int cellId, std::vector<Eigen::VectorXd> values, std::string name, UnstructuredGrid* grid);
 
 private:
--- src/visualize/Visualizer.h
+++ src/visualize/Visualizer.h
@@ -9,10 +9,16 @@ namespace NuTo
 namespace Visualize
 {
 
+//! Class to write visualization files of DOF values and cell data.
+//! @tparam THandler Cell handler class that converts computation cell into visualize output.
+//!                  For examples, see @link QuadAverageHandler @endlink and @link TensorProductVoronoiHandler @endlink.
 template <typename THandler>
 class Visualizer
 {
 public:
+    //! Construct a visualizer with a group of cells to be visualized.
+    //! @param cells Group of cells you want to visualize.
+    //! @param ...args Additional arguments that get passed on to the constructor of the cell handler.
     template<typename... HandlerArguments>
     Visualizer(Group<CellInterface>& cells, HandlerArguments&&... args)
         : mCells(cells)
@@ -22,6 +28,8 @@ public:
             mPointIds.push_back(mHandler.WriteGeometry(cell, &mGrid));
     }
 
+    //! Define DOF values that should be visualized.
+    //! @param dof DofType to visualize.
     void DofValues(DofType dof)
     {
         int i = 0;
@@ -32,6 +40,9 @@ public:
         }
     }
 
+    //! Define cell data that should be visualized.
+    //! @param f Function that is passed to the cell for evaluation.
+    //! @param name Name to be used in the resulting output file for the data array.
     void CellData(std::function<Eigen::VectorXd(const CellData&, const CellIpData&)> f, std::string name)
     {
         int i = 0;
@@ -43,6 +54,8 @@ public:
         }
     }
 
+    //! Write out a VTK file.
+    //! @param filename Name of the resulting file.
     void WriteVTKFile(std::string filename)
     {
         mGrid.ExportVtuDataFile(filename, false);
