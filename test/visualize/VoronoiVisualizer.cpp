cc5e7e26ece40e868b26ead47ba9328a1e393a90
peotto <peotto@gmail.com>
2018-01-08 11:35:50 +0100

    Add test for voronoi visualization of tensor product elements

--- test/visualize/VoronoiVisualizer.cpp
+++ test/visualize/VoronoiVisualizer.cpp
@@ -1,29 +1,26 @@
 #include "BoostUnitTest.h"
-#include "visualize/VoronoiVisualizer.h"
+#include "BoostUnitTest.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include "visualize/TensorProductVoronoiHandler.h"
+#include "visualize/Visualizer.h"
 #include "visualize/XMLWriter.h"
 #include "TestStructure.h"
-#include "visualize/CellGeometryVoronoi.h"
-#include "TensorProductGeometryVoronoi.h"
+
+namespace pt = boost::property_tree;
+using namespace NuTo;
 
 struct UnstructuredGridCheck
 {
 public:
-    static void Voronoi(const NuTo::Visualize::UnstructuredGrid& grid)
+    static void Voronoi(std::string filename)
     {
-        BOOST_CHECK_EQUAL(grid.mPoints.size(), 16 * 2);
-        BOOST_CHECK_EQUAL(grid.mCells.size(), 8);
-
-        BoostUnitTest::CheckEigenMatrix(grid.mCells[0].GetData(0), Eigen::Vector3d(1, 1, 1));
-        BoostUnitTest::CheckEigenMatrix(grid.mCells[0].GetData(1), Eigen::Vector3d(10, 10, 10));
-
-        BoostUnitTest::CheckEigenMatrix(grid.mCells[1].GetData(0), Eigen::Vector3d(2, 2, 2));
-        BoostUnitTest::CheckEigenMatrix(grid.mCells[1].GetData(1), Eigen::Vector3d(20, 20, 20));
-
-        BoostUnitTest::CheckEigenMatrix(grid.mCells[2].GetData(0), Eigen::Vector3d(3, 3, 3));
-        BoostUnitTest::CheckEigenMatrix(grid.mCells[2].GetData(1), Eigen::Vector3d(30, 30, 30));
-
-        BoostUnitTest::CheckEigenMatrix(grid.mCells[3].GetData(0), Eigen::Vector3d(4, 4, 4));
-        BoostUnitTest::CheckEigenMatrix(grid.mCells[3].GetData(1), Eigen::Vector3d(40, 40, 40));
+        pt::ptree tree;
+        pt::read_xml(filename, tree);
+        int numOfPoints = tree.get<int>("VTKFile.UnstructuredGrid.Piece.<xmlattr>.NumberOfPoints");
+        int numOfCells = tree.get<int>("VTKFile.UnstructuredGrid.Piece.<xmlattr>.NumberOfCells");
+        BOOST_CHECK_EQUAL(numOfPoints, 18);
+        BOOST_CHECK_EQUAL(numOfCells, 8);
     }
 };
 
@@ -31,20 +28,10 @@ BOOST_AUTO_TEST_CASE(GroupVoronoiTensorProduct2D)
 {
     NuTo::DofType dof("NodeCoordinatesDiv10", 2);
     NuTo::Test::VisualizeTestStructure s(dof);
+    auto cells = s.Cells();
 
-    NuTo::Visualize::CellGeometryVoronoi cellGeometry;
-
-    std::vector<double> points = {-0.5, 0.5};
-    NuTo::Test::TensorProductGeometryVoronoi<2>::buildCellGeometryVoronoi(points, cellGeometry);
-
-    NuTo::Visualize::GroupVoronoi v(s.Cells(), cellGeometry);
-    v.GeometryToGrid();
-    v.DofsToGrid({dof});
-    v.IpValuesToGrid();
-
-    const auto& grid = v.GetUnstructuredGrid();
-
-    UnstructuredGridCheck::Voronoi(grid);
-
-    NuTo::Visualize::XMLWriter::Export("PdeTensorVoronoiTest2D.vtu", grid, false);
-};
+    std::string filename = "VoronoiOutput.vtu";
+    Visualize::Visualizer<Visualize::TensorProductVoronoiHandler<2>> visualize(cells, 2);
+    visualize.WriteVTKFile(filename);
+    UnstructuredGridCheck::Voronoi(filename);
+}
