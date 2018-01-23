d968676cce70123740033c5859bcc6ff411f5ef5
Christoph Pohl <christophpohl@gmx.com>
2018-01-23 17:51:54 +0100

    Remove object libs from build system
    These were intended to avoid multiple compilation of the same file while
    still allowing building the unit tests without the finished library.
    However, it makes the build system quite complicated and prohibits using
    the CMake targets properly.

--- scripts/cmake/AddUnitTest.cmake
+++ scripts/cmake/AddUnitTest.cmake
@@ -1,39 +1,19 @@
-# `add_unit_test(SomeClass FilesItNeedsToLink)` builds the unit test of the
-# name `SomeClass` and all the files required (`FilesItNeedsToLink`), links
-# Boost unit test framework to it, and adds it to the test suite under an
+# `add_unit_test(SomeClass)` builds the unit test of the name `SomeClass`,
+# links it to the required libraries and adds it to the test suite under an
 # appropriate name
 function(add_unit_test ClassName)
     # find relative path, i.e. remove the `.../test/`
     string(REPLACE "${CMAKE_SOURCE_DIR}/test/" ""
         relpath ${CMAKE_CURRENT_SOURCE_DIR})
 
-    # if there are additional arguments, transform them from their filename
-    # to the appropriate object library targets
-    if(ARGN)
-        foreach(filename ${ARGN})
-            get_target_from_source(${CMAKE_SOURCE_DIR}/src/${filename} target)
-            set(AdditionalObjects
-                "${AdditionalObjects};$<TARGET_OBJECTS:${target}>")
-        endforeach()
-    endif()
-
-    # look for the corresponding object to the unit test
-    string(REPLACE "/" "." relpath ${relpath})
-    set(srcObject "${relpath}.${ClassName}")
-    # if there is an object (the class is not "header-only"), link it as well
-    if(TARGET "${srcObject}")
-        add_executable(${ClassName} ${ClassName}.cpp
-            $<TARGET_OBJECTS:${srcObject}> ${AdditionalObjects})
-    else()
-        add_executable(${ClassName} ${ClassName}.cpp
-            ${AdditionalObjects})
-    endif()
+    add_executable(${ClassName} ${ClassName}.cpp)
     # link the unit test framework to the unit test
     target_link_libraries(${ClassName} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
     target_include_directories(${ClassName} PRIVATE ${CMAKE_SOURCE_DIR}/src)
     target_include_directories(${ClassName}
         PRIVATE ${CMAKE_SOURCE_DIR}/test/tools)
     target_link_libraries(${ClassName} Fakeit)
+    target_link_libraries(${ClassName} Mechanics Base Math Visualize)
 
     # generate a ctest name for the test
     string(REPLACE "." "::" testname ${relpath})
--- scripts/cmake/GetTargetFromSource.cmake
+++
@@ -1,8 +0,0 @@
-# in three steps go from "/home/user/nuto/src/mechanics/MyClass.cpp"
-# to "mechanics.MyClass", and export this to Target
-function(get_target_from_source Source Target)
-    string(REPLACE "${CMAKE_SOURCE_DIR}/src/" "" libName ${Source})
-    string(REPLACE ".cpp" "" libName ${libName})
-    string(REPLACE "/" "." libName ${libName})
-    set(${Target} ${libName} PARENT_SCOPE)
-endfunction()
