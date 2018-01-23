e91daeb7b3fb32ea4d7ad68eb5edaa7c41ec8145
Christoph Pohl <christophpohl@gmx.com>
2018-01-23 17:38:43 +0100

    Add SuiteSparse solvers
    Curiosly, the CholmodSupernodalLLT seems to work without linking the
    matching library. Weird.

---
+++ scripts/cmake/FindSUITESPARSE.cmake
@@ -0,0 +1,14 @@
+# Creates IMPORTED target SuiteSparse::SuiteSparse
+message(STATUS "Checking for SuiteSparse Solver package ...")
+
+find_path(SUITESPARSE_INCLUDE_DIR NAMES umfpack.h HINTS /usr/include/suitesparse)
+
+find_library(SUITESPARSE_LIBRARIES NAMES umfpack)
+
+add_library(SuiteSparse::SuiteSparse SHARED IMPORTED)
+set_target_properties(SuiteSparse::SuiteSparse PROPERTIES
+    IMPORTED_LOCATION "${SUITESPARSE_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SUITESPARSE_INCLUDE_DIR}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SUITESPARSE DEFAULT_MSG SUITESPARSE_LIBRARIES SUITESPARSE_INCLUDE_DIR)
--- src/mechanics/solver/EigenSolver.cpp
+++ src/mechanics/solver/EigenSolver.cpp
@@ -3,6 +3,11 @@
 #include <Eigen/SparseLU>
 #include <Eigen/SparseQR>
 
+#ifdef HAS_SUITESPARSE
+#include <Eigen/UmfPackSupport>
+#include <Eigen/CholmodSupport>
+#endif
+
 namespace NuTo
 {
 
@@ -12,7 +17,7 @@ Eigen::VectorXd EigenSparseSolve(Eigen::SparseMatrix<double> A, Eigen::VectorXd 
     TSolver solver;
     solver.analyzePattern(A);
     solver.factorize(A);
-    return solver.solve(b); 
+    return solver.solve(b);
 }
 
 Eigen::VectorXd EigenSolver(Eigen::SparseMatrix<double> A, Eigen::VectorXd b, std::string solver)
@@ -21,6 +26,12 @@ Eigen::VectorXd EigenSolver(Eigen::SparseMatrix<double> A, Eigen::VectorXd b, st
         return EigenSparseSolve<Eigen::SparseLU<Eigen::SparseMatrix<double>>>(A, b);
     if (solver == "EigenSparseQR")
         return EigenSparseSolve<Eigen::SparseQR<Eigen::SparseMatrix<double>, Eigen::COLAMDOrdering<int>>>(A, b);
+#ifdef HAS_SUITESPARSE
+    if (solver == "EigenUmfPackLU")
+        return EigenSparseSolve<Eigen::UmfPackLU<Eigen::SparseMatrix<double>>>(A, b);
+    if (solver == "EigenCholmodSupernodalLLT")
+        return EigenSparseSolve<Eigen::CholmodSupernodalLLT<Eigen::SparseMatrix<double>>>(A, b);
+#endif
     throw Exception("Que?");
 }
 
