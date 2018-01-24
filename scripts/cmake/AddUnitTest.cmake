88394213665ed9dbb0ed4875964082754a37dd98
Christoph Pohl <christophpohl@gmx.com>
2018-01-24 09:54:30 +0100

    Remove linking of NuTo libraries from unit tests

--- scripts/cmake/AddUnitTest.cmake
+++ scripts/cmake/AddUnitTest.cmake
@@ -6,14 +6,24 @@ function(add_unit_test ClassName)
     string(REPLACE "${CMAKE_SOURCE_DIR}/test/" ""
         relpath ${CMAKE_CURRENT_SOURCE_DIR})
 
-    add_executable(${ClassName} ${ClassName}.cpp)
+    if(ARGN)
+        foreach(filename ${ARGN})
+            set(AdditionalSources "${AdditionalSources};${CMAKE_SOURCE_DIR}/src/${filename}")
+        endforeach()
+    endif()
+
+    set(CorrespondingCpp "${CMAKE_SOURCE_DIR}/src/${relpath}/${ClassName}.cpp")
+    if(EXISTS ${CorrespondingCpp})
+        add_executable(${ClassName} ${ClassName}.cpp ${CorrespondingCpp} ${AdditionalSources})
+    else()
+        add_executable(${ClassName} ${ClassName}.cpp ${AdditionalSources})
+    endif()
     # link the unit test framework to the unit test
     target_link_libraries(${ClassName} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
     target_include_directories(${ClassName} PRIVATE ${CMAKE_SOURCE_DIR}/src)
     target_include_directories(${ClassName}
         PRIVATE ${CMAKE_SOURCE_DIR}/test/tools)
     target_link_libraries(${ClassName} Fakeit)
-    target_link_libraries(${ClassName} Mechanics Base Math Visualize)
 
     # generate a ctest name for the test
     string(REPLACE "." "::" testname ${relpath})
