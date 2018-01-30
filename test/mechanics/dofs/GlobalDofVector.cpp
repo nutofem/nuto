38cdf490defc9770328adcf76434163951ea38d7
Thomas Titscher <thomas.titscher@gmail.com>
2018-01-30 14:07:39 +0100

    Add operator-= to GlobalDofVector

--- test/mechanics/dofs/GlobalDofVector.cpp
+++ test/mechanics/dofs/GlobalDofVector.cpp
@@ -4,22 +4,49 @@
 
 using namespace NuTo;
 
-BOOST_AUTO_TEST_CASE(Access)
+struct TestVector
 {
     GlobalDofVector v;
 
-    DofType dof0("d0", 1);
-    DofType dof1("d1", 1);
+    DofType dof0 = DofType("d0", 1);
+    DofType dof1 = DofType("d1", 1);
 
-    v.J[dof0] = Eigen::Vector3d(0, 1, 2);
-    v.K[dof0] = Eigen::Vector2d(3, 4);
+    DofVector<double> v0;
+    DofVector<double> v1;
 
-    v.J[dof1] = Eigen::Vector2d(10, 11);
-    v.K[dof1] = Eigen::Vector3d(12, 13, 14);
+    TestVector()
+    {
+        v.J[dof0] = Eigen::Vector3d(0, 1, 2);
+        v.K[dof0] = Eigen::Vector2d(3, 4);
 
+        v.J[dof1] = Eigen::Vector2d(10, 11);
+        v.K[dof1] = Eigen::Vector3d(12, 13, 14);
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(Access, TestVector)
+{
     for (int i = 0; i < 5; ++i)
     {
         BOOST_CHECK_CLOSE(v(dof0, i), i, 1.e-10);
         BOOST_CHECK_CLOSE(v(dof1, i), 10 + i, 1.e-10);
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(Calc, TestVector)
+{
+    v += v;
+    for (int i = 0; i < 5; ++i)
+    {
+        BOOST_CHECK_CLOSE(v(dof0, i), 2 * i, 1.e-10);
+        BOOST_CHECK_CLOSE(v(dof1, i), 2 * (10 + i), 1.e-10);
+    }
+
+    v -= v;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        BOOST_CHECK_SMALL(v(dof0, i), 1.e-10);
+        BOOST_CHECK_SMALL(v(dof1, i), 1.e-10);
     }
 }
