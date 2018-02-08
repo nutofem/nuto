9cab668eb5d5eda8aa4b472687d44c9ced9ec45a
Thomas Titscher <thomas.titscher@gmail.com>
2018-02-08 15:58:49 +0100

    Move QuasistaticProblem1D into a class

--- applications/integrationtests/mechanics/QuasistaticProblem1D.cpp
+++ applications/integrationtests/mechanics/QuasistaticProblem1D.cpp
@@ -24,150 +24,178 @@
 
 using namespace NuTo;
 
-auto ConstitutiveLaw(int numCells, int numIpsPerCell)
+
+class LocalDamageTruss
 {
+public:
+    LocalDamageTruss(int numElements)
+        : mMesh(UnitMeshFem::CreateLines(numElements))
+        , mDof("Dispacement", 1)
+        , mLaw(ConstitutiveLaw())
+        , mMomentumBalance(mDof, mLaw)
+        , mEquations(&mMesh)
+        , mProblem(mEquations, mDof)
+        , mIntegrationType(2, eIntegrationMethod::GAUSS)
+    {
+        AddDofInterpolation(&mMesh, mDof);
+        auto constraints = DefineConstraints(mMesh, mDof);
+        auto dofInfo = DofNumbering::Build(mMesh.NodesTotal(mDof), mDof, constraints);
+
+        mEquations.SetDofInfo(dofInfo);
+        mNumIndependentDofs = dofInfo.numIndependentDofs[mDof];
+        mProblem.SetConstraints(constraints, mNumIndependentDofs);
+        mProblem.mTolerance = 1.e-12;
+
+        mCellGroup = mCells.AddCells(mMesh.ElementsTotal(), mIntegrationType);
+        mLaw.mEvolution.ResizeHistoryData(mCellGroup.Size(), mIntegrationType.GetNumIntegrationPoints());
+
+
+        auto Gradient = Bind(mMomentumBalance, &Integrands::MomentumBalance<1>::Gradient);
+        auto Hessian0 = Bind(mMomentumBalance, &Integrands::MomentumBalance<1>::Hessian0);
+        CellInterface::VoidFunction UpdateHistory = [&](const CellData& cellData, const CellIpData& cellIpData) {
+            EngineeringStrain<1> strain = cellIpData.GetBMatrixStrain(mDof) * cellData.GetNodeValues(mDof);
+            mLaw.Update(strain, 0, cellData.GetCellId(), cellIpData.GetIpId());
+        };
+
+        mEquations.AddGradientFunction(mCellGroup, Gradient);
+        mEquations.AddHessian0Function(mCellGroup, Hessian0);
+        mEquations.AddUpdateFunction(mCellGroup, UpdateHistory);
+    }
+
+    void SetImperfection(double kappaImperfection)
+    {
+        const int imperfectionCell = mMesh.Elements.Size() / 2;
+        const int imperfectionIp = mLaw.mEvolution.Ip(imperfectionCell, 0);
+        mLaw.mEvolution.mKappas[imperfectionIp] = kappaImperfection;
+    }
+
+    void Solve(double tEnd)
+    {
+        Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> pureSolver;
+        auto solver = NewtonRaphson::CreateWrappedSolver(pureSolver);
+        Eigen::VectorXd x = Eigen::VectorXd::Zero(mNumIndependentDofs);
+
+        double globalTime = 0;
+        double timeStep = 0.01;
+        double maxTimeStep = 0.1;
+
+        int iStep = 0;
+        while (globalTime < tEnd && timeStep > 1.e-6)
+        {
+            std::cout << "Step " << iStep << " at t = " << globalTime << std::endl;
+            try
+            {
+                auto trialSystem = mProblem.TrialSystem(x, globalTime, timeStep);
+                Eigen::VectorXd trialX = x + solver.Solve(trialSystem.first, trialSystem.second);
+
+                mProblem.SetGlobalTime(globalTime + timeStep);
+
+                int numIterations = 0;
+                // Some solvers return values > 1e30 as valid solutions. Thus, we store the
+                // solution into tmpX (instead of x directly) and check afterwards.
+                Eigen::VectorXd tmpX =
+                        NewtonRaphson::Solve(mProblem, trialX, solver, 12, NewtonRaphson::LineSearch(), &numIterations);
+                if (tmpX.norm() > 1.e30)
+                    throw NewtonRaphson::NoConvergence("", "floating point exception");
+
+                mProblem.UpdateHistory(tmpX);
+
+                globalTime += timeStep;
+                x = tmpX;
+                iStep++;
+
+                std::cout << "Convergence after " << numIterations << " iterations.\n";
+                if (numIterations < 3)
+                {
+                    timeStep *= 1.5;
+                    timeStep = std::min(timeStep, maxTimeStep);
+                    std::cout << "--> Increasing time step to " << timeStep << ".\n";
+                }
+            }
+            catch (NewtonRaphson::NoConvergence& e)
+            {
+                timeStep *= 0.5;
+                std::cout << "No convergence!\n";
+                std::cout << "--> Reducing time step to " << timeStep << ".\n";
+            }
+        }
+    }
+
+    std::vector<double> DamageField()
+    {
+        auto Damage = [&](const CellData& cellData, const CellIpData& cellIpData) {
+            int ip = mLaw.mEvolution.Ip(cellData.GetCellId(), cellIpData.GetIpId());
+            double kappa = mLaw.mEvolution.mKappas[ip];
+            return Eigen::VectorXd::Constant(1, mLaw.mDamageLaw.Damage(kappa));
+        };
+
+        std::vector<double> damage;
+        for (auto& cell : mCellGroup)
+        {
+            auto ipDamage = cell.Eval(Damage);
+            for (auto dmg : ipDamage)
+                damage.push_back(dmg[0]);
+        }
+        return damage;
+    }
+
+private:
     using ElasticLaw = Laws::LinearElastic<1>;
     using DamageLaw = Constitutive::DamageLawExponential;
     using StrainNorm = Constitutive::ModifiedMisesStrainNorm<1>;
     using Evolution = Laws::EvolutionImplicit<1, StrainNorm>;
     using Law = Laws::LocalIsotropicDamage<1, DamageLaw, Evolution, ElasticLaw>;
 
-    double E = 20000.;
-    double nu = 0.2;
-    double ft = 4;
-    double fc = 10 * ft;
+    MeshFem mMesh;
 
-    double k0 = ft / E;
-    double alpha = 0.99;
-    double beta = 350;
+    DofType mDof;
+    Law mLaw;
+    Integrands::MomentumBalance<1> mMomentumBalance;
 
-    ElasticLaw elasticLaw(E, nu);
-    DamageLaw damageLaw(k0, beta, alpha);
-    StrainNorm strainNorm(nu, fc / ft);
-    Evolution evolution(strainNorm, numCells, numIpsPerCell);
+    EquationSystem mEquations;
+    QuasistaticProblem mProblem;
+    int mNumIndependentDofs;
 
-    return Law(damageLaw, evolution, elasticLaw);
-}
+    IntegrationTypeTensorProduct<1> mIntegrationType;
+    CellStorage mCells;
+    Group<CellInterface> mCellGroup;
 
-Constraint::Constraints DefineConstraints(MeshFem& mesh, DofType disp)
-{
-    auto nodesLeft = mesh.NodesAtAxis(eDirection::X, disp);
-    auto nodesRight = mesh.NodesAtAxis(eDirection::X, disp, 1);
-
-    Constraint::Constraints c;
-    c.Add(disp, Constraint::Component(nodesLeft, {eDirection::X}));
-    c.Add(disp, Constraint::Component(nodesRight, {eDirection::X}, Constraint::RhsRamp(1., 0.01)));
-    return c;
-}
-
-
-std::vector<double> DamageField1D(int numElements, double initialKappa)
-{
-    MeshFem mesh = UnitMeshFem::CreateLines(numElements);
-    DofType disp("Displacement", 1);
-    AddDofInterpolation(&mesh, disp);
-
-    auto constraints = DefineConstraints(mesh, disp);
-    auto dofInfo = DofNumbering::Build(mesh.NodesTotal(disp), disp, constraints);
-
-    CellStorage cells;
-    IntegrationTypeTensorProduct<1> integrationType(2, eIntegrationMethod::GAUSS);
-
-    auto cellGroup = cells.AddCells(mesh.ElementsTotal(), integrationType);
-    auto law = ConstitutiveLaw(cellGroup.Size(), integrationType.GetNumIntegrationPoints());
-
-    // impose imperfection:
-    const int imperfectionCell = numElements / 2;
-    const int imperfectionIp = law.mEvolution.Ip(imperfectionCell, 0);
-    law.mEvolution.mKappas[imperfectionIp] = initialKappa;
-
-    Integrands::MomentumBalance<1> momentumBalance(disp, law);
-
-    auto Gradient = Bind(momentumBalance, &Integrands::MomentumBalance<1>::Gradient);
-    auto Hessian0 = Bind(momentumBalance, &Integrands::MomentumBalance<1>::Hessian0);
-
-    CellInterface::VoidFunction UpdateHistory = [&](const CellData& cellData, const CellIpData& cellIpData) {
-        EngineeringStrain<1> strain = cellIpData.GetBMatrixStrain(disp) * cellData.GetNodeValues(disp);
-        law.Update(strain, 0, cellData.GetCellId(), cellIpData.GetIpId());
-    };
-
-    SimpleAssembler assembler(dofInfo.numIndependentDofs, dofInfo.numDependentDofs);
-    EquationSystem system(&assembler, &mesh);
-    system.AddGradientFunction(cellGroup, Gradient);
-    system.AddHessian0Function(cellGroup, Hessian0);
-    system.AddUpdateFunction(cellGroup, UpdateHistory);
-
-    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> pureSolver;
-    auto solver = NewtonRaphson::CreateWrappedSolver(pureSolver);
-    Eigen::VectorXd x = Eigen::VectorXd::Zero(dofInfo.numIndependentDofs[disp]);
-
-    double globalTime = 0;
-    double timeStep = 0.01;
-    double maxTimeStep = 0.1;
-
-    QuasistaticProblem problem(system, constraints, dofInfo.numIndependentDofs[disp], disp);
-    problem.mTolerance = 1.e-12;
-
-    int iStep = 0;
-    while (globalTime < 1 && timeStep > 1.e-6)
+    static Law ConstitutiveLaw()
     {
-        std::cout << "Step " << iStep << " at t = " << globalTime << std::endl;
-        try
-        {
-            auto trialSystem = problem.TrialSystem(x, globalTime, timeStep);
-            Eigen::VectorXd trialX = x + solver.Solve(trialSystem.first, trialSystem.second);
+        double E = 20000.;
+        double nu = 0.2;
+        double ft = 4;
+        double fc = 10 * ft;
 
-            problem.SetGlobalTime(globalTime + timeStep);
+        double k0 = ft / E;
+        double alpha = 0.99;
+        double beta = 350;
 
-            int numIterations = 0;
-            // Some solvers return values > 1e30 as valid solutions. Thus, we store the
-            // solution into tmpX (instead of x directly) and check afterwards.
-            Eigen::VectorXd tmpX =
-                    NewtonRaphson::Solve(problem, trialX, solver, 20., NewtonRaphson::LineSearch(), &numIterations);
-            if (tmpX.norm() > 1.e30)
-                throw NewtonRaphson::NoConvergence("", "floating point exception");
+        ElasticLaw elasticLaw(E, nu);
+        DamageLaw damageLaw(k0, beta, alpha);
+        StrainNorm strainNorm(nu, fc / ft);
+        Evolution evolution(strainNorm);
 
-            problem.UpdateHistory(tmpX);
-
-            globalTime += timeStep;
-            x = tmpX;
-            iStep++;
-
-            std::cout << "Convergence after " << numIterations << " iterations.\n";
-            if (numIterations < 3)
-            {
-                timeStep *= 1.5;
-                timeStep = std::min(timeStep, maxTimeStep);
-                std::cout << "--> Increasing time step to " << timeStep << ".\n";
-            }
-        }
-        catch (NewtonRaphson::NoConvergence& e)
-        {
-            timeStep *= 0.5;
-            std::cout << "No convergence!\n";
-            std::cout << "--> Reducing time step to " << timeStep << ".\n";
-        }
+        return Law(damageLaw, evolution, elasticLaw);
     }
 
-    auto Damage = [&](const CellData& cellData, const CellIpData& cellIpData) {
-        int ip = law.mEvolution.Ip(cellData.GetCellId(), cellIpData.GetIpId());
-        double kappa = law.mEvolution.mKappas[ip];
-        return Eigen::VectorXd::Constant(1, law.mDamageLaw.Damage(kappa));
-    };
-    std::vector<double> damage;
-    for (auto& cell : cellGroup)
+    Constraint::Constraints DefineConstraints(MeshFem& mesh, DofType disp)
     {
-        auto ipDamage = cell.Eval(Damage);
-        for (auto dmg : ipDamage)
-            damage.push_back(dmg[0]);
-    }
+        auto nodesLeft = mesh.NodesAtAxis(eDirection::X, disp);
+        auto nodesRight = mesh.NodesAtAxis(eDirection::X, disp, 1);
 
-    return damage;
-}
+        Constraint::Constraints c;
+        c.Add(disp, Constraint::Component(nodesLeft, {eDirection::X}));
+        c.Add(disp, Constraint::Component(nodesRight, {eDirection::X}, Constraint::RhsRamp(1., 0.01)));
+        return c;
+    }
+};
 
 BOOST_AUTO_TEST_CASE(LocalDamage1DWithoutImperfection)
 {
-    auto damageField = DamageField1D(5., 0.);
+    LocalDamageTruss problem(5);
+    problem.Solve(1);
+    auto damageField = problem.DamageField();
     // without an imperfection, we expect a uniform damage distribution.
     // If the trial state is messed up, we would have localization at
     // the displacement BC. (damage = 1 there, and damage = 0 everywhere else)
@@ -178,7 +206,10 @@ BOOST_AUTO_TEST_CASE(LocalDamage1DWithoutImperfection)
 
 BOOST_AUTO_TEST_CASE(LocalDamage1DWithImperfection)
 {
-    auto damageField = DamageField1D(5., 0.001);
+    LocalDamageTruss problem(5);
+    problem.SetImperfection(0.001);
+    problem.Solve(1);
+    auto damageField = problem.DamageField();
     // We expect localization in the two integration points
     // near the center
     size_t damagedIp1 = 4;
