b1c08e9c03b9ae3416cc19ef98910a975fd3a1fc
Christoph Pohl <christophpohl@gmx.com>
2018-03-02 15:55:56 +0100

    Seperate unit and integration test coverage report

--- scripts/upload_coverage.sh
+++ scripts/upload_coverage.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -ev
 # get coverage information
-lcov --capture --directory /home/nuto/build --output-file ../coverage.info # 2> /dev/null
+lcov --capture --directory /home/nuto/build --output-file ../coverage.info
 # filter out system stuff that we don't control
 lcov -r ../coverage.info '/usr/include/*' -o ../coverage.info
 # filter out external libraries
 lcov -r ../coverage.info '/home/nuto/source/external/*' -o ../coverage.info
-# upload to codecov
-bash <(curl -s https://codecov.io/bash) -f ../coverage.info # > /dev/null 2> /dev/null
+# upload to codecov, -F is the flag (unit or integration test)
+bash <(curl -s https://codecov.io/bash) -f ../coverage.info -F $1
+# delete gcda files so that they don't show up in next report
+find /home/nuto/build -name '*.gcda' -delete
