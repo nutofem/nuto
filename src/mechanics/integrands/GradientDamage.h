98c13724ea4eefbf33a51df18e3d25d2c3629452
Thomas Titscher <thomas.titscher@gmail.com>
2018-03-07 16:14:47 +0100

    Use LinearElasticDamage in GradientDamage

--- src/mechanics/integrands/GradientDamage.h
+++ src/mechanics/integrands/GradientDamage.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "mechanics/integrands/MomentumBalance.h"
-#include "mechanics/constitutive/LinearElastic.h"
+#include "mechanics/constitutive/LinearElasticDamage.h"
 #include "mechanics/constitutive/ModifiedMisesStrainNorm.h"
 
 namespace NuTo
@@ -26,24 +26,21 @@ public:
     //! @param disp dof type associated with displacements
     //! @param eeq scalar dof type associated with nonlocal equivalent strains
     //! @param c nonlocal parameter unit length squared
-    //! @param linearElasticLaw linear elastic law
+    //! @param linearElasticDamage damage law that avoids adding damage to the negative volumetric part
     //! @param damageLaw damage law that provides .Damage(double) and .Derivative(double)
     //! @param strainNorm modified mises strain norm
-    GradientDamage(DofType disp, ScalarDofType eeq, double c, Laws::LinearElastic<TDim> linearElasticLaw,
+    GradientDamage(DofType disp, ScalarDofType eeq, double c, Laws::LinearElasticDamage<TDim> linearElasticDamage,
                    TDamageLaw damageLaw, Constitutive::ModifiedMisesStrainNorm<TDim> strainNorm,
                    TInteraction interaction = TInteraction())
         : mDisp(disp)
         , mEeq(eeq)
         , mC(c)
-        , mElasticLaw(linearElasticLaw)
+        , mLinearElasticDamage(linearElasticDamage)
         , mDamageLaw(damageLaw)
         , mNorm(strainNorm)
         , mInteraction(interaction)
     {
     }
-
-    static constexpr double R = 0.005;
-    static constexpr double N = 5;
 
     virtual ~GradientDamage() = default;
 
@@ -63,7 +60,7 @@ public:
 
         double g = mInteraction.Factor(omega);
 
-        gradient[mDisp] = Bdisp.transpose() * ((1. - omega) * mElasticLaw.Stress(strain));
+        gradient[mDisp] = Bdisp.transpose() * mLinearElasticDamage.Stress(strain, omega);
         gradient[mEeq] = Neeq.transpose() * (eeq - mNorm.Value(strain)) + Beeq.transpose() * (mC * g * eeqGradient);
 
         return gradient;
@@ -88,12 +85,13 @@ public:
         double dgdw = mInteraction.Derivative(omega);
         auto eeqGradient = data.Apply(mEeq, Nabla::Gradient());
 
-        hessian0(mDisp, mDisp) = Bdisp.transpose() * ((1. - omega) * mElasticLaw.Tangent(strain)) * Bdisp;
+        hessian0(mDisp, mDisp) = Bdisp.transpose() * mLinearElasticDamage.DstressDstrain(strain, omega) * Bdisp;
         hessian0(mEeq, mDisp) = -Neeq.transpose() * mNorm.Derivative(strain).transpose() * Bdisp;
         hessian0(mEeq, mEeq) = Neeq.transpose() * Neeq + mC * g * Beeq.transpose() * Beeq +
                                Beeq.transpose() * mC * eeqGradient * dgdw * dOmega_dKappa * dKappa_dEeq * Neeq;
-        hessian0(mDisp, mEeq) =
-                Bdisp.transpose() * ((-dOmega_dKappa * dKappa_dEeq) * mElasticLaw.Stress(strain)) * Neeq;
+        hessian0(mDisp, mEeq) = Bdisp.transpose() *
+                                (mLinearElasticDamage.DstressDomega(strain, omega) * dOmega_dKappa * dKappa_dEeq) *
+                                Neeq;
 
         return hessian0;
     }
@@ -118,7 +116,7 @@ public:
     DofType mDisp;
     ScalarDofType mEeq;
     double mC;
-    Laws::LinearElastic<TDim> mElasticLaw;
+    Laws::LinearElasticDamage<TDim> mLinearElasticDamage;
     TDamageLaw mDamageLaw;
     Constitutive::ModifiedMisesStrainNorm<TDim> mNorm;
     TInteraction mInteraction;
