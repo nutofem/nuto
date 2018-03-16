3404a3fdd45b377022a4e99c8379929ce79bffda
Thomas Titscher <thomas.titscher@gmail.com>
2018-03-16 09:27:33 +0100

    Allow additional CLI args in add_integrationtest

--- scripts/cmake/AddIntegrationtest.cmake
+++ scripts/cmake/AddIntegrationtest.cmake
@@ -4,11 +4,18 @@ function(append_to_tests TestName)
 endfunction()
 
 function(add_integrationtest test)
+    if (ARGN) # additional arguments to this function are appended to the test
+              # command and represent additional args passed to the execuable
+        foreach (arg ${ARGN})
+            set(AdditionalArgs "${AdditionalArgs};${arg}")
+        endforeach()
+    endif()
+
     add_executable(${test} ${test}.cpp)
     target_link_libraries(${test} Mechanics Math Base Visualize 
         Boost::unit_test_framework ${LAPACK_LIBRARIES})
     target_include_directories(${test} PRIVATE ${CMAKE_SOURCE_DIR}/test/tools)
     get_filename_component(module ${CMAKE_CURRENT_SOURCE_DIR} NAME)
-    add_test(integration::${module}::${test}.cpp ${test})
+    add_test(integration::${module}::${test}.cpp ${test} ${AdditionalArgs})
     append_to_tests(${test})
 endfunction()
