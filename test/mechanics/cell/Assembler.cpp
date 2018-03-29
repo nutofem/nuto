31b0f612415019165ac0f3d92b73163e9534c727
Thomas Titscher <thomas.titscher@gmail.com>
2018-03-29 11:32:13 +0200

    Will automatically assemble into existing nonzeros
    After the first triplet list assembly

--- test/mechanics/cell/Assembler.cpp
+++ test/mechanics/cell/Assembler.cpp
@@ -37,24 +37,32 @@ BOOST_AUTO_TEST_CASE(MatrixAssembly)
     numbering0[d] = Eigen::Vector3i(0, 1, 2);
     numbering1[d] = Eigen::Vector3i(2, 3, 4);
 
-    DofContainer<int> size;
-    size[d] = 5;
-    MatrixAssembler matrixAssembler(size);
-    matrixAssembler.Add(m, numbering0);
-    matrixAssembler.Add(m, numbering1);
-
-    matrixAssembler.Finish();
-
     Eigen::MatrixXd expected(5, 5);
     expected.setZero();
     expected.block<3, 3>(0, 0) = m(d, d);
     expected.block<3, 3>(2, 2) = m(d, d);
     expected(2, 2) = 44;
 
+    DofContainer<int> size;
+    size[d] = 5;
+
+    MatrixAssembler matrixAssembler(size);
+    matrixAssembler.Add(m, numbering0);
+    matrixAssembler.Add(m, numbering1);
+    BOOST_CHECK_THROW(matrixAssembler.Get(), Exception); // no call to  .Finish()
+    matrixAssembler.Finish();
     BoostUnitTest::CheckEigenMatrix(Eigen::MatrixXd(matrixAssembler.Get()(d, d)), expected);
+
+    // Adding the same stuff again should result in two times the expected result.
+    // As the triplet lists (used in first assembly) are dropped after callling Finish(), assembling into triplet lists
+    // again should result in `expected` instead of `2 * expected`
+    matrixAssembler.Add(m, numbering0);
+    matrixAssembler.Add(m, numbering1);
+    BoostUnitTest::CheckEigenMatrix(Eigen::MatrixXd(matrixAssembler.Get()(d, d)), 2 * expected);
 
     matrixAssembler.Reset();
     BoostUnitTest::CheckEigenMatrix(Eigen::MatrixXd(matrixAssembler.Get()(d, d)), Eigen::MatrixXd::Zero(5, 5));
+
 
     auto matrixWithNonzeros = matrixAssembler.Get();
     MatrixAssembler::Add(matrixWithNonzeros, m, numbering0);
