313229381a6fb23a5b922907bb9bcf0e5a923901
Thomas Titscher <thomas.titscher@gmail.com>
2018-03-29 11:09:55 +0200

    Add MatrixAssembler

--- nuto/mechanics/cell/Assembler.h
+++ nuto/mechanics/cell/Assembler.h
@@ -14,11 +14,11 @@ std::vector<DofType> AvailableDofTypes(const DofVector<int> n, std::vector<DofTy
         return n.DofTypes();
 
     std::vector<DofType> intersection;
-    for (const auto& d1 : n.DofTypes())
-        for (const auto& d2 : dofTypes)
-            if (d1.Id() == d2.Id())
+    for (const auto& dofTypeRow : n.DofTypes())
+        for (const auto& dofTypeCol : dofTypes)
+            if (dofTypeRow.Id() == dofTypeCol.Id())
             {
-                intersection.push_back(d1);
+                intersection.push_back(dofTypeRow);
                 continue;
             }
     return intersection;
@@ -68,8 +68,75 @@ private:
 class MatrixAssembler
 {
 public:
+    MatrixAssembler(DofContainer<int> sizes = {})
+    {
+        Resize(sizes);
+    }
+
+    void Resize(DofContainer<int> sizes)
+    {
+        for (auto dofTypeRow : sizes)
+            for (auto dofTypeCol : sizes)
+                mMatrix(dofTypeRow.first, dofTypeCol.first).resize(dofTypeRow.second, dofTypeCol.second);
+    }
+
+    void Add(const DofMatrix<double>& m, const DofVector<int>& numbering, std::vector<DofType> dofTypes = {})
+    {
+        auto availableDofTypes = AvailableDofTypes(numbering, dofTypes);
+        for (const auto& dofTypeRow : availableDofTypes)
+        {
+            for (const auto& dofTypeCol : availableDofTypes)
+            {
+                auto& tripletListDof = mTriplets(dofTypeRow, dofTypeCol);
+                const auto& matrixDof = m(dofTypeRow, dofTypeCol);
+                const auto& numberingRow = numbering[dofTypeRow];
+                const auto& numberingCol = numbering[dofTypeCol];
+
+                for (unsigned localRow = 0; localRow < numberingRow.size(); ++localRow)
+                {
+                    for (unsigned localCol = 0; localCol < numberingCol.size(); ++localCol)
+                    {
+                        const int globalRow = numberingRow[localRow];
+                        const int globalCol = numberingCol[localCol];
+                        const double value = matrixDof(localRow, localCol);
+                        tripletListDof.push_back({globalRow, globalCol, value});
+                    }
+                }
+            }
+        }
+    }
+
+    void Reset()
+    {
+        for (auto dofTypeRow : mMatrix.DofTypes())
+            for (auto dofTypeCol : mMatrix.DofTypes())
+                mMatrix(dofTypeRow, dofTypeCol).setZero();
+    }
+
+    void Finish()
+    {
+        for (auto dofTypeRow : mMatrix.DofTypes())
+        {
+            for (auto dofTypeCol : mMatrix.DofTypes())
+            {
+                const auto& tripletListDof = mTriplets(dofTypeRow, dofTypeCol);
+                auto& matrix = mMatrix(dofTypeRow, dofTypeCol);
+                matrix.setFromTriplets(tripletListDof.begin(), tripletListDof.end());
+                matrix.makeCompressed();
+            }
+        }
+    }
+
+    const DofMatrixSparse<double>& Get() const
+    {
+        return mMatrix;
+    }
+
 private:
-    DofVector<double> mVector;
+    using TripletList = std::vector<Eigen::Triplet<double>>;
+    DofMatrixContainer<TripletList> mTriplets;
+
+    DofMatrixSparse<double> mMatrix;
 };
 
 
--- test/mechanics/cell/Assembler.cpp
+++ test/mechanics/cell/Assembler.cpp
@@ -24,3 +24,35 @@ BOOST_AUTO_TEST_CASE(VectorAssembly)
     vectorAssembler.Reset();
     BoostUnitTest::CheckVector(vectorAssembler.Get()[d], std::vector<double>{0, 0, 0, 0, 0}, 5);
 }
+
+
+BOOST_AUTO_TEST_CASE(MatrixAssembly)
+{
+    NuTo::DofType d("d", 1);
+    DofMatrix<double> m;
+    m(d, d).resize(3, 3);
+    m(d, d) << 11, 12, 13, 21, 22, 23, 31, 32, 33;
+
+    DofVector<int> numbering0, numbering1;
+    numbering0[d] = Eigen::Vector3i(0, 1, 2);
+    numbering1[d] = Eigen::Vector3i(2, 3, 4);
+
+    DofContainer<int> size;
+    size[d] = 5;
+    MatrixAssembler matrixAssembler(size);
+    matrixAssembler.Add(m, numbering0);
+    matrixAssembler.Add(m, numbering1);
+
+    matrixAssembler.Finish();
+
+    Eigen::MatrixXd expected(5, 5);
+    expected.setZero();
+    expected.block<3, 3>(0, 0) = m(d, d);
+    expected.block<3, 3>(2, 2) = m(d, d);
+    expected(2, 2) = 44;
+
+    BoostUnitTest::CheckEigenMatrix(Eigen::MatrixXd(matrixAssembler.Get()(d, d)), expected);
+
+    matrixAssembler.Reset();
+    BoostUnitTest::CheckEigenMatrix(Eigen::MatrixXd(matrixAssembler.Get()(d, d)), Eigen::MatrixXd::Zero(5, 5));
+}
