c9f22fc2fd6ed74fdb5dc1d26ea3206e39e993eb
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-04-27 14:57:29 +0200

    Add a comment [ci skip]

--- nuto/mechanics/tools/NewmarkSolver.cpp
+++ nuto/mechanics/tools/NewmarkSolver.cpp
@@ -113,6 +113,7 @@ DofVector<double> NewmarkSolver::TrialState(double newGlobalTime, const NuTo::Co
     //    return result;
 
 
+    missing trial state for v and a
     DofVector<double> trialU = mX[0] - result;
 
     return trialU;
