7bd1a22c4ba31021bad7d2802d2d8c8921bf2ffb
pmuelle2 <philipp.mueller2@bam.de>
2018-05-22 09:24:36 +0200

    Fix indentation

--- scripts/cmake/SetCompilerFlags.cmake
+++ scripts/cmake/SetCompilerFlags.cmake
@@ -8,9 +8,9 @@ macro(set_compiler_flags)
     set(PYTHON_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 
     # openmp specific
-      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-      set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
 
     # Clang specific options
     if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
